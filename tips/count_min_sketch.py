0c66dffb9be23a7a3b3fc8a678de73bec6006e37
Jeremy Kun <j2kun@users.noreply.github.com>
2021-05-04 21:02:58 -0700

    count_min_sketch: add heavy_hitters implementation

--- tips/count_min_sketch.py
+++ tips/count_min_sketch.py
@@ -1,5 +1,10 @@
-from typing import Callable
 from typing import Hashable
+from typing import Iterable
+from typing import List
+from typing import Set
+from typing import Tuple
+from typing import TypeVar
+import heapq
 import math
 import random
 
@@ -13,6 +18,7 @@ def random_hash(modulus):
 
     def f(x):
         return (a * x + b) % modulus
+
     return f
 
 
@@ -50,18 +56,75 @@ class CountSketch:
         return math.ceil(len(self) * self.accuracy)
 
 
+T = TypeVar("T")
+
+
+def heavy_hitters(data: Iterable[T], k: int) -> Set[T]:
+    '''Find the data entries that occur at least len(values) / k times.
+
+    This computes an approximate "heavy hitters" function, whereby it
+    guarantees to return all data entries that occur at least len(values) / k
+    times, but it may return some values that occur at least len(values) / (2k)
+    times. It is guaranteed with high probability not to return any values that
+    occur fewer than len(values) / (2k) times.
+
+    In exchange for this approximation, this method uses only O(k) space,
+    and O(log(k)) time per data entry.
+    '''
+    heap: List[Tuple[int, T]] = []
+    candidates: Set[T] = set()
+    count_processed = 0
+    sketch = CountSketch(accuracy=1 / (2 * k), confidence=1e-10)
+
+    def remove_heap_items_below(value):
+        while heap:
+            min_count, min_value = heapq.heappop(heap)
+            if min_count >= count_processed / k:
+                heapq.heappush(heap, (min_count, min_value))
+                return
+            candidates.remove(min_value)
+
+    for value in data:
+        count_processed += 1
+        sketch.increment(value)
+        est_count = sketch.count(value)
+
+        if est_count >= count_processed / k:
+            if value in candidates:
+                # This is actually O(k) time because Python's heapq doesn't
+                # support deleting an arbitrary element from the heap in log
+                # time. Implement a better heap to get the faster runtime.
+                heap = [(c, x) for (c, x) in heap if x != value]
+                heapq.heapify(heap)
+
+            if heap:
+                remove_heap_items_below(count_processed / k)
+            candidates.add(value)
+            heapq.heappush(heap, (est_count, value))
+
+    return set([x for (c, x) in heap])
+
+
 if __name__ == "__main__":
-    sketch = CountSketch()
-    for i in range(10):
-        sketch.increment(12)
-    for i in range(10000):
-        sketch.increment(i)
+    from collections import Counter
+    random.seed(1)
 
-    # perfect estimate would be 10
-    print(sketch.count(12))
+    n = 1000
+    k = 10
 
-    # actual estimate is more like 35
-    print(len(sketch))
+    # min output value has frequency n / 2k = 10
+    # guarantees all entries with at least n / k = 20 freq
 
-    # guarantee is "overestimated by at most 70"
-    print(sketch.additive_error_guarantee())
+    values = [-1] * (n // k) + [-2] * (n // (2 * k) + 1) + [-3] * (n // (2 * k) + 5)
+
+    remaining_space = n - len(values)
+    threshold = n // (2 * k) - 1
+    just_under_heavy = list(range(remaining_space // threshold)) * threshold
+    values = values + just_under_heavy
+    print(Counter(values))
+    random.shuffle(values)
+
+    # with this seed, it always chooses -1 (most frequent) but only chooses -2
+    # even though -3 is more frequent (though not above the threshold for
+    # guarantee)
+    print(heavy_hitters(values, k))
--- tips/count_min_sketch_test.py
+++ tips/count_min_sketch_test.py
@@ -1,13 +1,16 @@
 from collections import Counter
+from hypothesis import assume
 from hypothesis import given
 from hypothesis import settings
 from hypothesis.strategies import floats
 from hypothesis.strategies import integers
 from hypothesis.strategies import lists
-from hypothesis.strategies import text 
+from hypothesis.strategies import text
 import math
+import random
 
 from count_min_sketch import CountSketch
+from count_min_sketch import heavy_hitters
 
 
 @settings(deadline=2000)
@@ -15,7 +18,7 @@ from count_min_sketch import CountSketch
     lists(integers(), min_size=5),
     floats(min_value=1e-04, max_value=0.5, allow_nan=False, allow_infinity=False)
 )
-def test_error_guarantee_achieved(values, accuracy):
+def test_sketch_error_guarantee_achieved(values, accuracy):
     sketch = CountSketch(accuracy=accuracy, confidence=1e-10)
     for value in values:
         sketch.increment(value)
@@ -27,8 +30,37 @@ def test_error_guarantee_achieved(values, accuracy):
 
 
 @given(lists(text()))
-def test_len(values):
+def test_sketch_len(values):
     sketch = CountSketch(accuracy=0.01, confidence=0.01)
     for value in values:
         sketch.increment(value)
     assert len(sketch) == len(values)
+
+
+@given(integers(min_value=2, max_value=20))
+def test_heavy_hitters_guarantee_achieved(k):
+    n = 100
+    elements = list(range(1, n+1))
+    threshold = n // (2 * k)
+
+    heavy_hitter = elements[0]
+    almost_heavy = elements[1:3]
+
+    data = [heavy_hitter] * ((n // k) + 1)
+    data += almost_heavy * (threshold + 1)
+
+    underweight = elements[3:]
+    i = 0
+    while len(data) < n:
+        data.extend([underweight[i]] * min(threshold - 1, n - len(data)))
+        i += 1
+
+    data = data[:n]
+
+    random.shuffle(data)
+    output = heavy_hitters(data, k)
+
+    assert heavy_hitter in output, str(Counter(data))
+    for x in set(underweight):
+        assert x not in output, str(Counter(data))
+
