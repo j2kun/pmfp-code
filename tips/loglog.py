1139da6699046602d4b8ef4d68920eedf099ff9a
Jeremy Kun <j2kun@users.noreply.github.com>
2021-05-07 12:28:32 -0700

    add improved raw estimator

--- tips/loglog.py
+++ tips/loglog.py
@@ -1,4 +1,5 @@
-from hashlib import md5
+import random
+from collections import Counter
 from typing import Hashable
 from typing import Iterable
 
@@ -31,33 +32,92 @@ def random_hash(modulus):
     return f
 
 
-def geometric_mean_estimate(buckets):
-    '''Geometric mean of {m 2^b[0], ..., m 2^b[m-1]} with a bias correction
+class LogLog:
 
-    Simplified formula computing: 
+    def __init__(self, log_bucket_count: int = 14, estimator='improved'):
+        estimator_dict = {
+            'geometric': self.geometric_mean_estimate,
+            'harmonic': self.harmonic_mean_estimate,
+            'improved': self.improved_raw_estimate,
+        }
+        self.estimator_names = list(estimator_dict.keys())
+        self.estimator = estimator_dict[estimator]
 
-        (m 2^b[0] * ... * m 2^b[m-1])^(1/m)
+        self.log_bucket_count = log_bucket_count
+        self.buckets = [0] * (1 << log_bucket_count)
+        self.m = len(self.buckets)
+        self.non_index_bits = 64 - log_bucket_count
+        self.hash = random_hash(LARGE_RANDOM_PRIME)
 
-    The bias correction factor approximates e^(−γ) * √2 / 2,
-    where e is the Euler–Mascheroni constant γ ~ 0.5772156649...
-    '''
-    m = len(buckets)
-    return 0.39701181 * m * 2**(float(sum(buckets)) / m)
+    def add(self, value: Hashable) -> None:
+        hashed = self.hash(hash(value)) & ((1 << 64) - 1)
+        # unpack hashed value as (s, index), where index is the last
+        # log_bucket_count bits of the hash.
+        index = hashed & (len(self.buckets) - 1)
+        s = hashed >> self.log_bucket_count
+
+        # most of the published literature uses "the position of the first 1
+        # bit", rather than the number of trailing zeros. Choosing the latter
+        # has the effect of dividing the estimate by 2; To follow the
+        # literature, we use the position of the first 1 bit by adding 1 to the
+        # number of trailing zeros.
+        zeros = 1 + trailing_zeros(s, z=self.non_index_bits)
+        self.buckets[index] = max(self.buckets[index], zeros)
+
+    def add_all(self, data: Iterable[Hashable]) -> None:
+        for item in data:
+            self.add(item)
+
+    def cardinality(self) -> float:
+        return self.estimator()
+
+    def geometric_mean_estimate(self):
+        '''Geometric mean of {m 2^b[0], ..., m 2^b[m-1]} with a bias correction
+
+        Simplified formula computing: 
+
+            (m 2^b[0] * ... * m 2^b[m-1])^(1/m)
+
+        The bias correction factor approximates e^(−γ) * √2 / 2,
+        where e is the Euler–Mascheroni constant γ ~ 0.5772156649...
+        '''
+        return 0.39701181 * self.m * 2**(float(sum(self.buckets)) / self.m)
+
+    def harmonic_mean_estimate(self):
+        '''Harmonic mean of {m 2^b[0], ..., m 2^b[m-1]} with a bias correction
+         
+         Simplified formula computing:
+             m / ((1 / (m 2^b[0])) + ... + (1 / (m 2^b[m-1])))
+         
+         The correction factor is an approximation of 1 / (2 ln 2).
+         '''
+        return 0.72134752 * self.m**2 / sum(2**(-b) for b in self.buckets)
+
+    def improved_raw_estimate(self):
+        '''A modified harmonic mean, cf.  https://arxiv.org/abs/1702.01284.'''
+        q = self.non_index_bits
+
+        # Here we're counting the frequency of each run-length of trailing zeros,
+        # and as such the values in the counter are between 0 and q+1, inclusive.
+        # q+1 is the "default" when the entire register value is zero.
+        register_counts = Counter(self.buckets)
+        C = [register_counts[i] for i in range(0, q + 2)]
+
+        def sigma(x):
+            # The paper from the docstring remarks that 26 iterations is enough
+            return x + sum(x**(2**k) * 2**(k - 1) for k in range(1, 30))
+
+        def tau(x):
+            # The paper from the docstring remarks that 22 iterations is enough
+            return (1 - x - sum(2**(-k) * (1 - x**(2**(-k)))**2 for k in range(1, 30))) / 3
+
+        denom = sum(2**(-k) * C[k] for k in range(1, q + 1))
+        denom += self.m * sigma(C[0] / self.m)
+        denom += self.m * tau(1 - C[q + 1] / self.m) * 2**(-q)
+        return 0.72134752 * self.m**2 / denom
 
 
-def harmonic_mean_estimate(buckets):
-    '''Harmonic mean of {m 2^b[0], ..., m 2^b[m-1]} with a bias correction
-     
-     Simplified formula computing:
-         m / ((1 / (m 2^b[0])) + ... + (1 / (m 2^b[m-1])))
-     
-     The correction factor is an approximation of 1 / (2 ln 2).
-     '''
-    m = len(buckets)
-    return 0.72134752 * m**2 / sum(2**(-b) for b in buckets)
-
-
-def cardinality(data: Iterable[Hashable], log_bucket_count: int = 14) -> int:
+def cardinality(data: Iterable[Hashable], log_bucket_count: int = 14, estimator='improved') -> float:
     '''Estimate the number of unique items in the input dataset, up to 2^64.
 
     Arguments:
@@ -65,32 +125,12 @@ def cardinality(data: Iterable[Hashable], log_bucket_count: int = 14) -> int:
     - log_bucket_count: a value p such that the algorithm uses 2^p buckets, 
       each of which holds a 6 bit integer. 
     '''
-    buckets = [0] * (1 << log_bucket_count)
-    non_index_bits = 64 - log_bucket_count
-    rhash = random_hash(LARGE_RANDOM_PRIME)
-
-    for value in data:
-        hashed = rhash(hash(value)) & ((1 << 64) - 1)
-        # unpack hashed value as (s, index), where index is the last
-        # log_bucket_count bits of the hash.
-        index = hashed & (len(buckets) - 1)
-        s = hashed >> log_bucket_count
-
-        # most of the published literature uses "the position of the first 1
-        # bit", rather than the number of trailing zeros. Choosing the latter
-        # has the effect of dividing the estimate by 2; To follow the
-        # literature, we use the position of the first 1 bit by adding 1 to the
-        # number of trailing zeros.
-        zeros = 1 + trailing_zeros(s, z=non_index_bits)
-        buckets[index] = max(buckets[index], zeros)
-
-    return harmonic_mean_estimate(buckets)
+    hll = LogLog(log_bucket_count, estimator=estimator)
+    hll.add_all(data)
+    return hll.cardinality()
 
 
 if __name__ == "__main__":
-    import random
-    import cProfile
-
     def random_data(n=100000):
         return [random.random() for i in range(n)]
 
---
+++ tips/loglog_test.py
@@ -0,0 +1,40 @@
+import random
+import pytest
+
+from loglog import cardinality
+from loglog import LogLog
+
+
+estimator_names = LogLog(log_bucket_count=1).estimator_names
+
+
+def random_data(n=100000):
+    return [random.random() for i in range(n)]
+
+
+@pytest.mark.parametrize("estimator_name", estimator_names)
+def test_small_error_in_good_cardinality_range(estimator_name):
+    random.seed(1)
+
+    n = 100000
+    for i in range(5):
+      result = cardinality(random_data(n=n), estimator=estimator_name) / n
+
+      assert abs(1 - result) < 0.04
+
+
+def test_improved_estimator_works_on_all_cardinalities():
+    random.seed(1)
+
+    for n in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+      result = cardinality(random_data(n=n), estimator='improved') / n
+      assert abs(1 - result) < 0.04
+
+
+@pytest.mark.parametrize("estimator_name", [x for x in estimator_names if x != 'improved'])
+def test_normal_estimators_are_poor_on_small_cardinalities(estimator_name):
+    random.seed(1)
+
+    for n in [1, 10, 100, 1000, 10000]:
+      result = cardinality(random_data(n=n), estimator=estimator_name) / n
+      assert abs(1 - result) > 0.04
