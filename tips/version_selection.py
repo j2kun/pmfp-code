64e773ce3dfeee65bf68c2ee04e5cd021eb4d1df
Jeremy Kun <j2kun@users.noreply.github.com>
2021-05-29 14:34:06 -0700

    Add SAT solver for version selection problem (#16)
    * omit pysat from mypy
    
    * add first draft of version_selection SAT solver
    
    * add more exhaustive testing
    
    * add python-sat to requirements

---
+++ tips/version_selection.py
@@ -0,0 +1,98 @@
+from collections import deque
+from dataclasses import dataclass
+from itertools import combinations
+from typing import Callable
+from typing import Dict
+from typing import Iterable
+from typing import Tuple
+
+from pysat.solvers import Minisat22
+
+
+Name = str
+Version = int
+
+
+@dataclass(frozen=True)
+class PackageVersion:
+    package_id: int  # unique positive int, for indexing variables
+    name: Name
+    version: Version
+    dependencies: Iterable[Tuple[Name, Iterable[Version]]]
+
+    def as_tuple(self):
+        return (self.name, self.version)
+
+
+NameIndex = Callable[[Name], Dict[Version, PackageVersion]]
+IdIndex = Callable[[int], PackageVersion]
+
+
+def select_dependent_versions(
+    package: PackageVersion,
+    package_index: NameIndex,
+    id_index: IdIndex,
+) -> Dict[Name, Version]:
+    '''Select versions of dependent packages to install, or report impossible.'''
+    solver = Minisat22()
+
+    clauses = []
+    to_process = deque([package])
+    processed = set()
+
+    while to_process:
+        next_package = to_process.pop()
+        processed.add(next_package)
+
+        '''
+        At most one version of any package may be installed.  While SAT 
+        solvers admit many ways to model "cardinality constraints", we
+        choose a simple "pairwise" model that adds the constraint
+
+            v1 => NOT v2
+
+        for two distinct versions v1, v2, of the same package. 
+        This is equivalent to
+
+            (NOT v1) OR (NOT v2)
+        '''
+        versions = [
+            p.package_id
+            for p in package_index(next_package.name).values()
+        ]
+        for (v1, v2) in combinations(versions, 2):
+            clauses.append([-v1, -v2])
+
+        '''
+        For each dependency DEP, we need a clause that says 
+        "if next_package is chosen, one of DEP's allowed versions 
+        must be installed."
+
+            next_package => (DEP_v1 OR DEP_v2 OR ...)
+
+        This is equivalent to
+
+            (!next_package OR DEP_v1 OR DEP_v2 OR ...)
+        '''
+        for name, allowed_versions in next_package.dependencies:
+            clause = [-next_package.package_id]
+            for version in allowed_versions:
+                dep = package_index(name)[version]
+                clause.append(dep.package_id)
+                if dep not in processed:
+                    to_process.appendleft(dep)
+            clauses.append(clause)
+
+    for clause in clauses:
+        solver.add_clause(clause)
+
+    # solve with the assumption that the input package is chosen
+    if not solver.solve(assumptions=[package.package_id]):
+        # print(clauses)
+        # return solver.get_core()
+        raise ValueError("Infeasible!")
+
+    return dict(
+        id_index(abs(v)).as_tuple()
+        for v in solver.get_model() if v > 0
+    )
---
+++ tips/version_selection_test.py
@@ -0,0 +1,276 @@
+from collections import defaultdict
+from hypothesis import assume
+from hypothesis import given
+from hypothesis.strategies import booleans
+from hypothesis.strategies import composite
+from hypothesis.strategies import integers
+from itertools import groupby
+from itertools import takewhile
+import pytest
+
+from version_selection import PackageVersion
+from version_selection import select_dependent_versions
+
+
+class IdIndex:
+    def __init__(self, packages):
+        self.id_index = dict((p.package_id, p) for p in packages)
+
+    def __str__(self):
+        return str(self.id_index)
+
+    def __repr__(self):
+        return str(self.id_index)
+
+    def __call__(self, package_id):
+        return self.id_index[package_id]
+
+
+class NameVerIndex:
+    def __init__(self, packages):
+        self.name_ver_index = dict(
+            (key, dict((p.version, p) for p in group))
+            for (key, group) in groupby(packages, lambda p: p.name)
+        )
+
+    def __str__(self):
+        return str(self.name_ver_index)
+
+    def __repr__(self):
+        return str(self.name_ver_index)
+
+    def __call__(self, name):
+        return self.name_ver_index[name]
+
+
+def build_indices(packages):
+    return (
+        NameVerIndex(packages),
+        IdIndex(packages),
+    )
+
+
+def test_no_deps():
+    package = PackageVersion(
+        package_id=1,
+        name='foo',
+        version=1,
+        dependencies=tuple(),
+    )
+    chosen = select_dependent_versions(
+        package,
+        *build_indices([package])
+    )
+
+    assert chosen == {'foo': 1}
+
+
+def test_single_dep():
+    package = PackageVersion(
+        package_id=1,
+        name='foo',
+        version=1,
+        dependencies=(
+            ('bar', (1,)),
+        ),
+    )
+    dep = PackageVersion(
+        package_id=2,
+        name='bar',
+        version=1,
+        dependencies=tuple(),
+    )
+    chosen = select_dependent_versions(
+        package,
+        *build_indices([package, dep])
+    )
+
+    assert chosen == {'foo': 1, 'bar': 1}
+
+
+def test_branch_deps():
+    p1 = PackageVersion(
+        package_id=1,
+        name='foo',
+        version=1,
+        dependencies=(
+            ('bar', (1,)),
+            ('baz', (1,)),
+        ),
+    )
+    p2 = PackageVersion(
+        package_id=2,
+        name='bar',
+        version=1,
+        dependencies=tuple(),
+    )
+    p3 = PackageVersion(
+        package_id=3,
+        name='baz',
+        version=1,
+        dependencies=tuple(),
+    )
+    chosen = select_dependent_versions(
+        p1,
+        *build_indices([p1, p2, p3])
+    )
+
+    assert chosen == {'foo': 1, 'bar': 1, 'baz': 1}
+
+
+def test_diamond_deps_feasible():
+    p1 = PackageVersion(
+        package_id=1,
+        name='foo',
+        version=1,
+        dependencies=(
+            ('bar', (1,)),
+            ('baz', (1,)),
+        ),
+    )
+    p2 = PackageVersion(
+        package_id=2,
+        name='bar',
+        version=1,
+        dependencies=(
+            ('quux', (1, 2,)),
+        ),
+    )
+    p3 = PackageVersion(
+        package_id=3,
+        name='baz',
+        version=1,
+        dependencies=(
+            ('quux', (1,)),
+        ),
+    )
+    p4 = PackageVersion(
+        package_id=4,
+        name='quux',
+        version=1,
+        dependencies=tuple(),
+    )
+    p5 = PackageVersion(
+        package_id=5,
+        name='quux',
+        version=2,
+        dependencies=tuple(),
+    )
+    chosen = select_dependent_versions(
+        p1,
+        *build_indices([p1, p2, p3, p4, p5])
+    )
+
+    assert chosen == {'foo': 1, 'bar': 1, 'baz': 1, 'quux': 1}
+
+
+def test_diamond_deps_infeasible():
+    p1 = PackageVersion(
+        package_id=1,
+        name='foo',
+        version=1,
+        dependencies=(
+            ('bar', (1,)),
+            ('baz', (1,)),
+        ),
+    )
+    p2 = PackageVersion(
+        package_id=2,
+        name='bar',
+        version=1,
+        dependencies=(
+            ('quux', (1,)),
+        ),
+    )
+    p3 = PackageVersion(
+        package_id=3,
+        name='baz',
+        version=1,
+        dependencies=(
+            ('quux', (2,)),
+        ),
+    )
+    p4 = PackageVersion(
+        package_id=4,
+        name='quux',
+        version=1,
+        dependencies=tuple(),
+    )
+    p5 = PackageVersion(
+        package_id=5,
+        name='quux',
+        version=2,
+        dependencies=tuple(),
+    )
+
+    with pytest.raises(ValueError):
+        select_dependent_versions(
+            p1,
+            *build_indices([p1, p2, p3, p4, p5])
+        )
+
+
+@composite
+def dependency_graph(
+    draw,
+    dependency_decider=booleans(),
+    num_packages=integers(min_value=1, max_value=10),
+    num_versions=integers(min_value=1, max_value=10)
+):
+    names = [f'package_{i}' for i in range(draw(num_packages))]
+    packages = []
+    i = 1
+    for name in names:
+        previous_names = list(takewhile(lambda n: n != name, names))
+        index = NameVerIndex(packages)
+        for version in range(1, 1 + draw(num_versions)):
+            dependencies = []
+            for dep_name in previous_names:
+                if draw(dependency_decider):
+                    legal_dep_versions = []
+                    for dep_version in list(index(dep_name).keys()):
+                        if draw(dependency_decider):
+                            legal_dep_versions.append(dep_version)
+                    if legal_dep_versions:
+                        dependencies.append((dep_name, tuple(legal_dep_versions)))
+
+            packages.append(PackageVersion(
+                package_id=i,
+                name=name,
+                version=version,
+                dependencies=tuple(dependencies),
+            ))
+            i += 1
+
+    return packages
+
+
+@given(dependency_graph())
+def test_arbitrary_dep_graph_feasible(graph):
+    try:
+        chosen = select_dependent_versions(
+            graph[-1], *build_indices(graph)
+        )
+    except:
+        chosen = dict()
+
+    # either infeasible or no deps on the last package
+    assume(chosen != dict())
+
+    index = NameVerIndex(graph)
+    for (name, version) in chosen.items():
+        p = index(name)[version]
+        for dep_name, allowed_versions in p.dependencies:
+            assert dep_name in chosen
+            assert chosen[dep_name] in allowed_versions
+
+
+@given(dependency_graph())
+def test_arbitrary_dep_graph_infeasible(graph):
+    try:
+        chosen = select_dependent_versions(
+            graph[-1], *build_indices(graph)
+        )
+        assume(False)
+    except:
+        chosen = dict()
