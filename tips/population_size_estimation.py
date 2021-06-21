8279a063a0bed289c4ef84065a0094414e4541a7
Jeremy Kun <j2kun@users.noreply.github.com>
2021-06-20 22:19:12 -0700

    Population estimation (#18)
    * add first draft of code
    
    * add hypothesis test
    
    * add confidence interval
    
    * add caveat about sampling with replacement

---
+++ tips/population_size_estimation.py
@@ -0,0 +1,45 @@
+from typing import List
+from typing import Tuple
+
+
+def estimate_size(sample: List[int]) -> float:
+    '''
+    Estimate the size of a set {1, 2, ..., n}, given a uniform random sample
+    of its members without replacement.
+
+    Args:
+      - sample: a uniform random subset of the population
+
+    Returns: 
+      an estimate of the population size
+    '''
+    k = len(sample)
+    m = max(sample)
+    return m + m / k - 1
+
+
+def size_confidence_interval(sample: List[int], confidence: float) -> Tuple[float, float]:
+    '''Return a confidence interval analogue of estimate_size.
+
+    This computes the c-percent downward-biased confidence interval [0, c] of
+    the size of a population given a uniform random sample of its members
+    with replacement.
+
+    Caveat: this formula uses sampling with replacement, whereas the interval
+    for sampling without replacement is slightly narrower given the same
+    confidence level. For practical purposes you can probably ignore the
+    difference.
+
+    Args:
+      - sample: a uniform random subset of the population
+      - confidence: a float 0 < c < 1
+
+    Returns: 
+      A confidence interval [m, B*m] representing a (100*c)% confidence
+      interval around the true underlying population size, where m is the
+      maximum observation in the sample.
+    '''
+    k = len(sample)
+    m = max(sample)
+    B = 1 / confidence ** (1 / k)
+    return (m, B * m)
---
+++ tips/population_size_estimation_test.py
@@ -0,0 +1,42 @@
+from hypothesis import given
+from hypothesis import settings
+from hypothesis.strategies import floats
+from hypothesis.strategies import integers
+import math
+import random
+
+from population_size_estimation import estimate_size
+from population_size_estimation import size_confidence_interval
+
+
+@given(integers(min_value=100, max_value=10000), floats(min_value=0.2, max_value=0.9))
+@settings(deadline=1000)
+def test_estimate_is_accurate(n, sample_frac):
+    failures = 0
+    counterexamples = []
+    subjects = list(range(n))
+    attempts = 20
+
+    for i in range(attempts):
+        sample = random.sample(subjects, int(n * sample_frac))
+
+        low, high = size_confidence_interval(sample, 0.01)
+        success = low <= n <= high
+        if not success:
+            failures += 1
+            counterexamples.append([n, 'confidence_interval', low, high])
+
+        actual = estimate_size(sample)
+        # the variance is n^2 / k^2, so the stddev is n/k, and we should expect to
+        # be within 4 standard deviations of the true value.
+        stddev = n / len(sample)
+        threshold = 4 * stddev
+        success = abs(actual - n) < threshold
+
+        if not success:
+            failures += 1
+            counterexamples.append([n, 'point_estimate', actual, threshold])
+
+    if counterexamples:
+        print(counterexamples)
+        assert failures / attempts <= 0.1
