a370b37e509b33202ba15c7718bc3b13688347db
Jeremy Kun <j2kun@users.noreply.github.com>
2021-10-22 14:32:45 -0700

    gray_code: add implementation

--- tips/gray_code.py
+++ tips/gray_code.py
@@ -1,11 +1,20 @@
+from typing import Iterator
+from typing import Optional
+from dataclasses import dataclass
 
 
 def to_gray_code(x: int) -> int:
-    return (x >> 1)^x
+    '''Convert a counter index to its corresponding Gray code.'''
+    return (x >> 1) ^ x
+
 
 def from_gray_code(n: int) -> int:
-    # This implementation is from John D. Cook
-    # Reproduced with permission 
+    '''
+    Convert a Gray code to its corresponding index in the sequence of
+    Gray code iteration.
+    '''
+    # This implementation is from John D. Cook, reproduced with
+    # permission.
     # https://www.johndcook.com/blog/2020/09/08/inverse-gray-code/
     x, e = n, 1
     while x:
@@ -13,3 +22,42 @@ def from_gray_code(n: int) -> int:
         e *= 2
         n = n ^ x
     return n
+
+
+@dataclass
+class SettingsChange:
+    '''The numbers of binary settings.'''
+    num_settings: int
+
+    '''
+    The choice of values for the settings, as a bit mask of length
+    num_settings.
+    '''
+    settings: int
+
+    '''Which bit was flipped last.'''
+    flipped_bit: Optional[int]
+
+
+def gray_code_iter(num_settings: int) -> Iterator[SettingsChange]:
+    '''
+    Iterate over the range 1..2^num_settings, where each step in the
+    iteration modifies only a single bit at a time.
+    '''
+    i = 0
+    next_value = to_gray_code(i)
+    yield SettingsChange(
+        num_settings=num_settings, 
+        settings=next_value, 
+        flipped_bit=None
+    )
+
+    for i in range(1, 2 ** num_settings):
+        last_value = next_value
+        next_value = to_gray_code(i)
+        flipped_bit = (next_value ^ last_value).bit_length() - 1
+        yield SettingsChange(
+            num_settings=num_settings,
+            settings=next_value,
+            flipped_bit=flipped_bit
+        )
--- tips/gray_code_test.py
+++ tips/gray_code_test.py
@@ -1,7 +1,9 @@
 from hypothesis import given
 from hypothesis.strategies import integers
 
+from gray_code import SettingsChange
 from gray_code import from_gray_code
+from gray_code import gray_code_iter
 from gray_code import to_gray_code
 
 
@@ -12,3 +14,30 @@ def test_convert_to_gray_code():
 @given(integers(min_value = 0))
 def test_convert_and_back(x):
     assert x == from_gray_code(to_gray_code(x))
+
+
+def test_iter():
+    n = 4
+    settings = lambda n, s, f: SettingsChange(
+            num_settings=n, settings=s, flipped_bit=f)
+
+    expected = [
+        settings(n, 0, None),
+        settings(n, 1, 0),
+        settings(n, 3, 1),
+        settings(n, 2, 0),
+        settings(n, 6, 2),
+        settings(n, 7, 0),
+        settings(n, 5, 1),
+        settings(n, 4, 0),
+        settings(n, 12, 3),
+        settings(n, 13, 0),
+        settings(n, 15, 1),
+        settings(n, 14, 0),
+        settings(n, 10, 2),
+        settings(n, 11, 0),
+        settings(n, 9, 1),
+        settings(n, 8, 0),
+    ]
+
+    assert expected == list(gray_code_iter(n))
