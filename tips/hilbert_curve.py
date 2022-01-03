bf96fb36d387523a34193f11fada6ee34563737b
Jeremy Kun <j2kun@users.noreply.github.com>
2022-01-03 14:52:01 -0800

    hilbert_curve: add iteration
    Also attempt a comparison between naive matrix-vector multiplication
    and the hilbert-ordered version. Results are not totally consistent, but
    roughly 1% improvement when using 2**11-sized matrix of random ints.
    
    ```
    initial data generation: 5.510976076126099s
    hilbert data preprocessing: 7.537955045700073s
    Naive: 20.154295319s (1.0077147659499999s per)
    Hilbert: 20.013448998999998s (1.00067244995s per)
    Improvement: 0.6988402113331182%
    ```

--- tips/hilbert_curve.py
+++ tips/hilbert_curve.py
@@ -1,18 +1,24 @@
-from typing import Tuple
-from typing import NewType
+'''Algorithms for converting 2D coordinates to and from the Hilbert index.
 
-HilbertIndex = NewType('HilbertIndex', int)
-Coordinates = NewType('Coordinates', Tuple[int, int])
+Here the Hilbert curve has been scaled and discretized, so that the
+range {0, 1, ..., n^2 - 1} is mapped to coordinates 
+{0, 1, ..., n-1} x {0, 1, ..., n-1}. In the classical Hilbert curve,
+the continuous interval [0,1] is mapped to the unit square [0,1]^2.
+'''
+import time
+import math
+
+from collections import deque
+from typing import Deque
+from typing import Iterator
+from typing import Tuple
+
+HilbertIndex = int
+Coordinates = Tuple[int, int]
 
 
 def to_coordinates(index: HilbertIndex, n: int) -> Coordinates:
-    '''
-    Convert a Hilbert index to a 2d-coordinate.
-
-    Here the Hilbert curve has been scaled and discretized, so that the
-    range {0, 1, ..., n^2 - 1} is mapped to coordinates 
-    {0, 1, ..., n-1} x {0, 1, ..., n-1}. In the classical Hilbert curve,
-    the continuous interval [0,1] is mapped to the unit square [0,1]^2.
+    '''Convert a Hilbert index to a 2d-coordinate.
 
     Args:
       - index: the Hilbert-curve index of the point
@@ -20,7 +26,7 @@ def to_coordinates(index: HilbertIndex, n: int) -> Coordinates:
            coordinates.
 
     Returns: 
-      the (x,y) coordinate of the corresponding data point.
+      The (x,y) coordinate of the corresponding data point.
     '''
     s, t = (1, int(index))
     x, y = (0, 0)
@@ -34,35 +40,149 @@ def to_coordinates(index: HilbertIndex, n: int) -> Coordinates:
         t //= 4
         s *= 2
 
-    return Coordinates((x, y))
+    return (x, y)
 
 
 def to_hilbert_index(coords: Coordinates, n: int) -> HilbertIndex:
-    '''
-    Convert 2d-coordinates to the Hilbert index.
+    '''Convert 2d-coordinates to the Hilbert index.
+
+    Args:
+      - coords: the 2D coordinates of a data point.
+      - n: a power of 2 representing the width of the square grid of
+           coordinates.
+
+    Returns: 
+      The Hilbert index of the data point.
     '''
     rx, ry, s, t = (0, 0, n // 2, 0)
     (x, y) = coords
 
     while s > 0:
-        rx = 1 if (x & s) > 0 else 0
-        ry = 1 if (y & s) > 0 else 0
+        rx = int((x & s) > 0)
+        ry = int((y & s) > 0)
         t += s * s * ((3 * rx) ^ ry)
         (x, y) = rotate(n, x, y, rx, ry)
         s //= 2
 
-    return HilbertIndex(t)
+    return t
 
 
 def rotate(n: int, x: int, y: int, rx: int, ry: int) -> Tuple[int, int]:
     if (ry == 0):
         if (rx == 1):
-            x = n - 1 - x
-            y = n - 1 - y
-
-        # Swap x and y
-        t = x
-        x = y
-        y = t
+            (x, y) = (n - 1 - x, n - 1 - y)
+        (x, y) = (y, x)
 
     return (x, y)
+
+
+def naive_matrix_vector_product(A, v, output, n):
+    for i in range(n):
+        for j in range(n):
+            output[i] += A[i][j] * v[j]
+
+
+def hilbert_matrix_vector_product(flattened_A, v, output, coordinate_iter):
+    for t, (i, j) in coordinate_iter:
+        output[i] += flattened_A[t] * v[j]
+
+
+def hilbert_iter(depth: int) -> Iterator[Coordinates]:
+    queue: Deque[Tuple[str, int]] = deque([('H', depth)])
+    i, j = 0, 0
+    non_terminals = set('HABC')
+    yield (i, j)
+
+    while queue:
+        (symbol, depth) = queue.popleft()
+        if depth == 0 and symbol not in non_terminals:
+            if symbol == '↑':
+                i += 1
+            if symbol == '↓':
+                i -= 1
+            if symbol == '→':
+                j += 1
+            if symbol == '←':
+                j -= 1
+            yield (i, j)
+        if depth > 0:
+            if symbol == 'H':
+                queue.append(('A', depth - 1))
+                queue.append(('↑', depth - 1))
+                queue.append(('H', depth - 1))
+                queue.append(('→', depth - 1))
+                queue.append(('H', depth - 1))
+                queue.append(('↓', depth - 1))
+                queue.append(('B', depth - 1))
+            elif symbol == 'A':
+                queue.append(('H', depth - 1))
+                queue.append(('→', depth - 1))
+                queue.append(('A', depth - 1))
+                queue.append(('↑', depth - 1))
+                queue.append(('A', depth - 1))
+                queue.append(('←', depth - 1))
+                queue.append(('C', depth - 1))
+            elif symbol == 'B':
+                queue.append(('C', depth - 1))
+                queue.append(('←', depth - 1))
+                queue.append(('B', depth - 1))
+                queue.append(('↓', depth - 1))
+                queue.append(('B', depth - 1))
+                queue.append(('→', depth - 1))
+                queue.append(('H', depth - 1))
+            elif symbol == 'C':
+                queue.append(('B', depth - 1))
+                queue.append(('↓', depth - 1))
+                queue.append(('C', depth - 1))
+                queue.append(('←', depth - 1))
+                queue.append(('C', depth - 1))
+                queue.append(('↑', depth - 1))
+                queue.append(('A', depth - 1))
+            else:
+                # terminal up/down/left/right symbols
+                # must be preserved until the end
+                queue.append((symbol, depth - 1))
+
+
+if __name__ == '__main__':
+    import random
+    import timeit
+    random.seed(10)
+    n = 2**11
+    start = time.time()
+    A = [[random.randint(1, 10) for _ in range(n)] for _ in range(n)]
+    v = [random.randint(1, 10) for _ in range(n)]
+    output1 = [0] * n
+    output2 = [0] * n
+    end = time.time()
+    print(f'initial data generation: {end-start}s')
+
+    timeit_count = 20
+    total_n_seconds = timeit.timeit(
+        lambda: naive_matrix_vector_product(A, v, output1, n),
+        number=timeit_count
+    )
+
+    # reorder data
+    start = time.time()
+
+    flattened_A = [0] * (n * n)
+    depth = int(math.log2(n))
+    coordinate_iter = list(enumerate(hilbert_iter(depth)))
+    for (t, (i, j)) in coordinate_iter:
+        flattened_A[t] = A[i][j]
+
+    end = time.time()
+    print(f'hilbert data preprocessing: {end-start}s')
+
+    total_h_seconds = timeit.timeit(
+        lambda:
+        hilbert_matrix_vector_product(flattened_A, v, output2, coordinate_iter),
+        number=timeit_count
+    )
+
+    assert output1 == output2
+
+    print(f'Naive: {total_n_seconds}s ({total_n_seconds/timeit_count}s per)')
+    print(f'Hilbert: {total_h_seconds}s ({total_h_seconds/timeit_count}s per)')
+    print(f'Improvement: {100 * (1 - total_h_seconds / total_n_seconds)}%')
--- tips/hilbert_curve_test.py
+++ tips/hilbert_curve_test.py
@@ -4,8 +4,7 @@ from hypothesis.strategies import integers
 
 from hilbert_curve import to_coordinates
 from hilbert_curve import to_hilbert_index
-from hilbert_curve import HilbertIndex
-from hilbert_curve import Coordinates
+from hilbert_curve import hilbert_iter
 
 
 @composite
@@ -15,7 +14,7 @@ def dimension_and_hilbert_index(
     log_dim = draw(log_dim)
     dim = 2**log_dim
     index = draw(integers(min_value=0, max_value=dim - 1))
-    return (dim, HilbertIndex(index))
+    return (dim, index)
 
 
 @composite
@@ -26,7 +25,7 @@ def dimension_and_coordinates(
     dim = 2**log_dim
     x = draw(integers(min_value=0, max_value=dim - 1))
     y = draw(integers(min_value=0, max_value=dim - 1))
-    return (dim, Coordinates((x, y)))
+    return (dim, (x, y))
 
 
 @given(dimension_and_hilbert_index())
@@ -45,3 +44,40 @@ def test_left_inverse_composes_to_identity(dim_and_coords):
     index = to_hilbert_index(coords, dim)
     actual = to_coordinates(index, dim)
     assert actual == coords
+
+
+def test_hilbert_iter_16():
+    depth = 2  # depth d -> 4^d cells, in this case it's 16 = 4^2
+    expected = [
+        (0, 0),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (2, 0),
+        (3, 0),
+        (3, 1),
+        (2, 1),
+        (2, 2),
+        (3, 2),
+        (3, 3),
+        (2, 3),
+        (1, 3),
+        (1, 2),
+        (0, 2),
+        (0, 3),
+    ]
+    actual = list(hilbert_iter(depth))
+    assert len(actual) == len(expected)
+    assert expected == actual
+
+
+# TODO: fix this by reimplementing the to_coordinates and from_coordinates
+# to match the iteration order of hilbert_iter
+'''
+@given(integers(min_value=0, max_value=6))
+def test_hilbert_iter_uses_ascending_index_order(log_dim):
+    dim = 2**log_dim
+    coords_iter = hilbert_iter(log_dim)
+    hilbert_index_iter = [to_hilbert_index(x, dim) for x in coords_iter]
+    assert hilbert_index_iter == list(range(dim))
+'''
