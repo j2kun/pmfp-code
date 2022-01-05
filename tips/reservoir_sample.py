d130cf1a9b3141422d5cf94e787fc603d0b36c0b
Jeremy Kun <j2kun@users.noreply.github.com>
2022-01-05 10:53:17 -0800

    Add code for reservoir sampling (#22)
    * reservoir: add first two impls
    
    * reservoir: add weighted variant

---
+++ tips/reservoir_sample.py
@@ -0,0 +1,83 @@
+import math
+import random
+
+from typing import Iterable
+from typing import List
+from typing import Tuple
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+def sample_without_replacement(sample_size: int,
+                               stream: Iterable[T]) -> List[T]:
+    '''Sample without replacement from a stream.
+
+    The classical, but not optimally efficient reservoir sampling algorithm.
+    '''
+    chosen = []
+    for i, element in enumerate(stream):
+        if i < sample_size:
+            chosen.append(element)
+        else:
+            j = random.randint(0, i)
+            if j < sample_size:
+                chosen[j] = element
+
+    return chosen
+
+
+def algorithm_L(sample_size: int, stream: Iterable[T]) -> List[T]:
+    '''Sample without replacement from a stream.
+
+    From Li 1994, "Reservoir-sampling algorithms of time complexity O(n(1 + log(N/n)))"
+
+    This algorithm requires the 
+    '''
+    stream_iter = iter(stream)
+    chosen = [next(stream_iter) for _ in range(sample_size)]
+    W = math.exp(math.log(random.random()) / sample_size)
+
+    while True:
+        try:
+            jump_ahead = math.floor(math.log(random.random()) / math.log(1 - W))
+            # Python does not support "jumping ahead" in a generator efficiently
+            # so this simulates that operation.
+            for _ in range(jump_ahead):
+                next(stream_iter)
+
+            chosen[random.randint(0, sample_size - 1)] = next(stream_iter)
+            W = W * math.exp(math.log(random.random()) / sample_size)
+        except StopIteration:
+            return chosen
+
+
+def weighted_sample_with_replacement(
+        sample_size: int,
+        stream: Iterable[Tuple[T, float]]) -> List[T]:
+    '''Sample with replacement from a stream according to item weights.
+
+    This version demonstrates two variants: sampling with replacement instead
+    of without replacement, and sampling with probability proportional to
+    weights attached to (or quickly computable from) each stream item.
+
+    Instead of replacing a random element, we allow each element to be independently
+    replaced.
+    '''
+    chosen: List[T] = []
+    cumulative_weight = 0.0
+    for (element, weight) in stream:
+        cumulative_weight += weight
+        if cumulative_weight == 0:
+            continue
+
+        if not chosen:
+            chosen = [element for _ in range(sample_size)]
+        else:
+            for i in range(sample_size):
+                choice = random.random()
+                if choice < weight / cumulative_weight:
+                    chosen[i] = element
+
+    return chosen
+
---
+++ tips/reservoir_sample_test.py
@@ -0,0 +1,138 @@
+from collections import defaultdict
+import itertools
+import math
+import random
+
+import pytest
+
+from reservoir_sample import algorithm_L
+from reservoir_sample import sample_without_replacement
+from reservoir_sample import weighted_sample_with_replacement
+
+
+# used to assert the expected probability of each outcome
+def uniform_random_without_repacement(n, k):
+    return 1.0 / math.comb(n, k)
+
+
+def uniform_random_with_repacement(n, k):
+    return 1.0 / (n**k)
+
+
+def total_variation_distance(d1, d2):
+    return max(abs(d1[result] - d2[result]) for result in d1)
+
+
+@pytest.mark.parametrize(
+    "sampler", [
+        sample_without_replacement,
+        algorithm_L,
+    ]
+)
+def test_reservoir_sample_probability(sampler):
+    random.seed(1)
+    n = 10
+    sample_size = 3
+    stream = list(range(n))
+    expected_weight = uniform_random_without_repacement(n, sample_size)
+
+    counter = defaultdict(int)
+    experiment_count = 100000
+
+    for _ in range(experiment_count):
+        result = tuple(sorted(sampler(sample_size, stream)))
+        counter[result] += 1
+
+    assert len(counter) == int(1 / expected_weight)
+
+    expected = dict(
+        ((i, j, k), expected_weight)
+        for (i, j, k) in itertools.combinations(stream, sample_size)
+    )
+    actual = defaultdict(
+        int,
+        ((result, counter[result] / experiment_count) for result in counter)
+    )
+
+    total_variation = total_variation_distance(expected, actual)
+    assert total_variation < 0.01
+
+
+def test_weighted_sample_with_replacement_uniform_weights():
+    sampler = weighted_sample_with_replacement
+    random.seed(1)
+    n = 10
+    sample_size = 3
+    expected_weight = uniform_random_with_repacement(n, sample_size)
+    stream = [(k, 1.0 / n) for k in range(n)]
+
+    counter = defaultdict(int)
+    experiment_count = 100000
+
+    for _ in range(experiment_count):
+        # Important we don't sort the tuples here, because each (unsorted)
+        # tuple should occur with equal probability.
+        result = tuple(sampler(sample_size, stream))
+        counter[result] += 1
+
+    assert len(counter) == int(1 / expected_weight)
+
+    expected = dict(
+        ((i, j, k), expected_weight) for (i, _) in stream for (j, _) in stream
+        for (k, _) in stream
+    )
+    actual = defaultdict(
+        int,
+        ((result, counter[result] / experiment_count) for result in counter)
+    )
+    total_variation = total_variation_distance(expected, actual)
+    assert total_variation < 0.01
+
+
+def test_weighted_sample_with_replacement_point_distribution():
+    sampler = weighted_sample_with_replacement
+    random.seed(1)
+    n = 10
+    sample_size = 3
+    expected_weight = uniform_random_with_repacement(n, sample_size)
+
+    expected = 5
+    stream = [(k, 1.0 if k == expected else 0) for k in range(n)]
+
+    counter = defaultdict(int)
+    experiment_count = 1000
+
+    for _ in range(experiment_count):
+        # Important we don't sort the tuples here, because each (unsorted)
+        # tuple should occur with equal probability.
+        result = sampler(sample_size, stream)
+        assert result == [expected] * sample_size
+
+
+def test_weighted_sample_with_replacement_nonuniform_weights():
+    sampler = weighted_sample_with_replacement
+    random.seed(1)
+    n = 8
+    sample_size = 3
+    stream = [(k, k) for k in range(n)]
+    counter = defaultdict(int)
+    experiment_count = 100000
+
+    for _ in range(experiment_count):
+        result = tuple(sampler(sample_size, stream))
+        counter[result] += 1
+
+    weight_sum = sum(x[1] for x in stream)
+    expected = dict(
+        ((i, j, k), i_weight * j_weight * k_weight / weight_sum**sample_size)
+        for (i, i_weight) in stream for (j, j_weight) in stream
+        for (k, k_weight) in stream
+    )
+
+    actual = defaultdict(
+        int,
+        ((result, counter[result] / experiment_count) for result in counter)
+    )
+
+    total_variation = total_variation_distance(expected, actual)
+    assert total_variation < 0.01
