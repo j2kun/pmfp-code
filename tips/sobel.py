0a088318d47c6f6f48c06f30f25387f75f495644
Jeremy Kun <j2kun@users.noreply.github.com>
2022-01-07 13:37:38 -0800

    Code for Sobel filter tip (#23)
    * sobel: add impl shell
    
    * add config for isort
    
    * winding_number: formatting
    
    * sobel: add generic convolve impl
    
    * sobel: add inlined impl
    
    * sobel: finish main impl
    
    * sobel: add two example figures
    
    * mypy: add PIL to mypy ignore
    
    * gitignore: ignore mypy daemon file
    
    * sobel: add module docstring

---
+++ tips/sobel.py
@@ -0,0 +1,157 @@
+'''Edge detection using the Sobel kernel.
+
+This module contains three implementations:
+
+    - A pure-python, generic convolution operator. It is quite slow.
+    - A pure-python, loop-unrolled implementation of Sobel convolution.
+    - A faster numpy-based implementation using a generic numpy-based
+      convolution.
+
+Running this file via `python tips/sobel.py path/to/file.jpg` applies the Sobel
+kernel to an image, and saves the resulting image to disk.
+'''
+from itertools import product
+from typing import List
+
+import numpy as np
+
+Matrix = List[List[int]]
+
+
+SOBEL_HORIZONTAL_KERNEL = [
+    [1, 0, -1],
+    [2, 0, -2],
+    [1, 0, -1],
+]
+SOBEL_VERTICAL_KERNEL = [
+    [1, 2, 1],
+    [0, 0, 0],
+    [-1, -2, -1],
+]
+
+
+def indices(num_rows: int, num_cols: int):
+    '''Return a generator over a 2D index space in row-major order.
+
+    A pure-python version of the (much faster) numpy.ndindex.
+    '''
+    return product(range(num_rows), range(num_cols))
+
+
+def detect_edges(image: Matrix) -> Matrix:
+    '''Detect the edges in an image.
+
+    Arguments:
+      - image: a 2D array containing one color channel of the image to process
+
+    Returns:
+      A 2D array. Entries (i, j) with large magnitude detect the presence of an edge
+      at image position (i+1, j+1).
+    '''
+    G_x = convolve(image, SOBEL_HORIZONTAL_KERNEL)
+    G_y = convolve(image, SOBEL_VERTICAL_KERNEL)
+
+    # sum of absolute values is an approximation of the norm, sqrt(x^2 + y^2)
+    return [
+        [abs(x) + abs(y) for (x, y) in zip(row_G_x, row_G_y)]
+        for (row_G_x, row_G_y) in zip(G_x, G_y)
+    ]
+
+
+def convolve(matrix: Matrix, kernel: Matrix) -> Matrix:
+    '''Convolve a matrix with a kernel.
+
+    Arguments:
+      - matrx: a 2D array to be convolved
+      - kernel: a smaller 2D array to use as the convolution kernel.
+
+    Returns:
+      A 2D array. Entries (i, j) contain the sum
+          sum_{i_2, j_2} (kernel[i_2][j_j] * matrix[i+i_2][j+j_2])
+    '''
+    output_num_rows, output_num_cols = (
+        len(matrix) - len(kernel) + 1,
+        len(matrix[0]) - len(kernel[0]) + 1,
+    )
+    kernel_indices = list(indices(len(kernel), len(kernel[0])))
+
+    output = [[0] * output_num_cols for _ in range(output_num_rows)]
+    for (row, col) in indices(output_num_rows, output_num_cols):
+        value = 0
+        for (i, j) in kernel_indices:
+            value += kernel[i][j] * matrix[row + i][col + j]
+        output[row][col] = value
+
+    return output
+
+
+def sobel_optimized(matrix: Matrix) -> Matrix:
+    # Use numpy or a compiled language if you want it to be truly fast. This
+    # simply unrolls the generic convolution loop for the Sobel operators.
+    output_num_rows, output_num_cols = (len(matrix) - 2, len(matrix[0]) - 2)
+
+    output = [[0] * output_num_cols for _ in range(output_num_rows)]
+    for (row, col) in indices(output_num_rows, output_num_cols):
+        m00 = matrix[row][col]
+        m01 = matrix[row][col + 1]
+        m02 = matrix[row][col + 2]
+        m10 = matrix[row + 1][col]
+        m12 = matrix[row + 1][col + 2]
+        m20 = matrix[row + 2][col]
+        m21 = matrix[row + 2][col + 1]
+        m22 = matrix[row + 2][col + 2]
+        x = m00 - m02 + 2 * (m10 - m12) + m20 - m22
+        y = m00 - m20 + 2 * (m01 - m21) + m02 - m22
+        output[row][col] = abs(x) + abs(y)
+
+    return output
+
+
+def np_convolve2d(matrix: np.ndarray, kernel: np.ndarray):
+    '''An optimized 2d convolution routine using numpy.
+
+    Kudos to the many answers on StackOverflow describing the use of
+    as_strided and einsum.
+    '''
+    sub_shape = kernel.shape
+    view_shape = tuple(np.subtract(matrix.shape, sub_shape) + 1) + sub_shape
+    strides = 2 * matrix.strides
+    sub_matrix = np.lib.stride_tricks.as_strided(matrix, shape=view_shape, strides=strides)
+    return np.einsum('kl,ijkl->ij', kernel, sub_matrix)
+
+
+def np_detect_edges(image: np.ndarray) -> np.ndarray:
+    '''A numpy version of detect_edges.'''
+    np_horizontal = np.array(SOBEL_HORIZONTAL_KERNEL)
+    np_vertical = np.array(SOBEL_VERTICAL_KERNEL)
+    G_x = np_convolve2d(image, np_horizontal)
+    G_y = np_convolve2d(image, np_vertical)
+    return np.abs(G_x) + np.abs(G_y)
+
+
+if __name__ == '__main__':
+    import argparse
+    import os
+
+    from PIL import Image
+
+    parser = argparse.ArgumentParser(description='Apply the Sobel filter to an image.')
+    parser.add_argument('source_file')
+    args = parser.parse_args()
+
+    with Image.open(args.source_file) as im:
+        # Convert to grayscale, one value per pixel. Could alternatively
+        # compute Sobel on each color channel and recombine.
+        pixel_array = np.array(im.convert('L'))
+        print(pixel_array.shape)
+
+    print('detecting edges')
+    sobel_pixel_array = np_detect_edges(pixel_array)
+    sobel_pixel_array = np.floor(255 * sobel_pixel_array / np.max(sobel_pixel_array))
+
+    print('converting back to image')
+    edge_image = Image.fromarray(sobel_pixel_array.astype(np.uint8), mode='L')
+    # edge_image.show()
+    source_dir, source_name = os.path.split(args.source_file)
+    outfile = os.path.join(source_dir, f'edges_{source_name}')
+    edge_image.save(outfile)
---
+++ tips/sobel_test.py
@@ -0,0 +1,170 @@
+from hypothesis import given
+from hypothesis.strategies import composite
+from hypothesis.strategies import integers
+from hypothesis.strategies import lists
+import numpy as np
+import pytest
+from sobel import convolve
+from sobel import detect_edges
+from sobel import np_convolve2d
+from sobel import np_detect_edges
+from sobel import sobel_optimized
+
+
+def test_convolve_4by4_with_2by2():
+    matrix = [
+        [1, 2, 3, 4],
+        [2, -3, 4, -1],
+        [3, 3, 0, 2],
+        [1, 9, -2, 2],
+    ]
+    kernel = [
+        [1, 2],
+        [-1, -2],
+    ]
+    expected = [
+        [9, 3, 9],
+        [-13, 2, -2],
+        [-10, -2, 2],
+    ]
+    assert convolve(matrix, kernel) == expected
+
+
+def test_np_convolve_4by4_with_2by2():
+    matrix = np.array([
+        [1, 2, 3, 4],
+        [2, -3, 4, -1],
+        [3, 3, 0, 2],
+        [1, 9, -2, 2],
+    ])
+    kernel = np.array([
+        [1, 2],
+        [-1, -2],
+    ])
+    expected = np.array([
+        [9, 3, 9],
+        [-13, 2, -2],
+        [-10, -2, 2],
+    ])
+    actual = np_convolve2d(matrix, kernel)
+    np.testing.assert_array_equal(actual, expected)
+
+
+@composite
+def random_matrix_and_kernel(draw, min_dim=1, max_dim=10):
+    """Generate a matrix, and a kernel with strictly smaller dimension."""
+    matrix_dim = integers(
+        min_value=min_dim,
+        max_value=max_dim,
+    )
+    values = integers(min_value=-100, max_value=100)
+    matrix_row_count = draw(matrix_dim)
+    matrix_col_count = draw(matrix_dim)
+    matrix = draw(
+        lists(
+            lists(values,
+                  min_size=matrix_col_count,
+                  max_size=matrix_col_count),
+            min_size=matrix_row_count,
+            max_size=matrix_row_count
+        ))
+
+    kernel_row_count = draw(integers(
+        min_value=1,
+        max_value=matrix_row_count,
+    ))
+    kernel_col_count = draw(integers(
+        min_value=1,
+        max_value=matrix_col_count,
+    ))
+    kernel = draw(
+        lists(
+            lists(values,
+                  min_size=kernel_col_count,
+                  max_size=kernel_col_count),
+            min_size=kernel_row_count,
+            max_size=kernel_row_count
+        ))
+
+    return (matrix, kernel)
+
+
+@given(random_matrix_and_kernel(min_dim=1, max_dim=10))
+def test_dimension_fuzz_test(matrix_and_kernel):
+    # This test merely runs the convolve on matrices and kernels of varying
+    # dimensions, to check whether the dimension calculations are off.
+    matrix, kernel = matrix_and_kernel
+    result = convolve(matrix, kernel)
+    assert len(result) == len(matrix) - len(kernel) + 1
+    assert len(result[0]) == len(matrix[0]) - len(kernel[0]) + 1
+
+
+@given(random_matrix_and_kernel(min_dim=1, max_dim=10))
+def test_np_dimension_fuzz_test(matrix_and_kernel):
+    # This test merely runs the convolve on matrices and kernels of varying
+    # dimensions, to check whether the dimension calculations are off.
+    matrix, kernel = matrix_and_kernel
+    matrix, kernel = np.array(matrix), np.array(kernel)
+    result = np_convolve2d(matrix, kernel)
+    assert len(result) == len(matrix) - len(kernel) + 1
+    assert len(result[0]) == len(matrix[0]) - len(kernel[0]) + 1
+
+
+@pytest.mark.parametrize("detect_edges_fn", [detect_edges, sobel_optimized])
+def test_detect_edges_simple_vert(detect_edges_fn):
+    matrix = [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+    ]
+    expected = [
+        [0, 4],
+        [0, 4],
+    ]
+    assert detect_edges_fn(matrix) == expected
+
+
+@pytest.mark.parametrize("detect_edges_fn", [detect_edges, sobel_optimized])
+def test_detect_edges_simple_horizontal(detect_edges_fn):
+    matrix = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+    ]
+    expected = [
+        [4, 4],
+        [0, 0],
+    ]
+    assert detect_edges_fn(matrix) == expected
+
+
+@composite
+def random_matrix(draw, min_dim=1, max_dim=10):
+    """Generate a matrix, and a kernel with strictly smaller dimension."""
+    matrix_dim = integers(
+        min_value=min_dim,
+        max_value=max_dim,
+    )
+    values = integers(min_value=-100, max_value=100)
+    matrix_row_count = draw(matrix_dim)
+    matrix_col_count = draw(matrix_dim)
+    matrix = draw(
+        lists(
+            lists(values,
+                  min_size=matrix_col_count,
+                  max_size=matrix_col_count),
+            min_size=matrix_row_count,
+            max_size=matrix_row_count
+        ))
+    return matrix
+
+
+@given(random_matrix(min_dim=4, max_dim=20))
+def test_consistency_between_impls(matrix):
+    result1 = detect_edges(matrix)
+    result2 = sobel_optimized(matrix)
+    result3 = np_detect_edges(np.array(matrix))
+    assert result1 == result2
+    np.testing.assert_array_equal(np.array(result1), result3)
