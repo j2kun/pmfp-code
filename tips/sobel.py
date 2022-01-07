5be5e1f2bbbadb828276eafa77b11141c8b17153
Jeremy Kun <j2kun@users.noreply.github.com>
2022-01-06 20:39:31 -0800

    sobel: add generic convolve impl

--- tips/sobel.py
+++ tips/sobel.py
@@ -1,11 +1,46 @@
+from itertools import product
+from typing import List
+
+Matrix = List[List[int]]
 
 
-# use numpy?
-SOBEL_KERNEL = []
+SOBEL_HORIZONTAL_KERNEL = [
+    [1, 0, -1],
+    [2, 0, -2],
+    [1, 0, -1],
+]
+SOBEL_VERTICAL_KERNEL = [
+    [1, 2, 1],
+    [0, 0, 0],
+    [-1, -2, -1],
+]
 
 
-def sobel(image):
-    return convolve(image, SOBEL_KERNEL)
+def indices(num_rows: int, num_cols: int):
+    return product(range(num_rows), range(num_cols))
 
-def convolve(matrix, kernel):
-    pass
+
+def detect_edges(image: Matrix) -> Matrix:
+    G_x = convolve(image, SOBEL_HORIZONTAL_KERNEL)
+    G_y = convolve(image, SOBEL_VERTICAL_KERNEL)
+    return [
+        [abs(x) + abs(y) for (x, y) in zip(row_G_x, row_G_y)]
+        for (row_G_x, row_G_y) in zip(G_x, G_y)
+    ]
+
+
+def convolve(matrix: Matrix, kernel: Matrix) -> Matrix:
+    output_num_rows, output_num_cols = (
+        len(matrix) - len(kernel) + 1,
+        len(matrix[0]) - len(kernel[0]) + 1,
+    )
+    kernel_indices = list(indices(len(kernel), len(kernel[0])))
+
+    output = [[0] * output_num_cols for _ in range(output_num_rows)]
+    for (row, col) in indices(output_num_rows, output_num_cols):
+        value = 0
+        for (i, j) in kernel_indices:
+            value += kernel[i][j] * matrix[row + i][col + j]
+        output[row][col] = value
+
+    return output
--- tips/sobel_test.py
+++ tips/sobel_test.py
@@ -1,7 +1,12 @@
+from hypothesis import given
+from hypothesis.strategies import composite
+from hypothesis.strategies import integers
+from hypothesis.strategies import lists
 from sobel import convolve
+from sobel import detect_edges
 
 
-def test_convolve_3_by_3():
+def test_convolve_4by4_with_2by2():
     matrix = [
         [1, 2, 3, 4],
         [2, -3, 4, -1],
@@ -12,6 +17,85 @@ def test_convolve_3_by_3():
         [1, 2],
         [-1, -2],
     ]
-    # TODO: add expected output
-    expected = []
+    expected = [
+        [9, 3, 9],
+        [-13, 2, -2],
+        [-10, -2, 2],
+    ]
     assert convolve(matrix, kernel) == expected
+
+
+@composite
+def random_matrix_and_kernel(draw, min_dim=1, max_dim=10):
+    """Generate a matrix, and a kernel with strictly smaller dimension."""
+    matrix_dim = integers(
+        min_value=min_dim,
+        max_value=max_dim,
+    )
+    values = integers(min_value=-100, max_value=100)
+    matrix_row_count = draw(matrix_dim)
+    matrix_col_count = draw(matrix_dim)
+    matrix = draw(
+        lists(
+            lists(values,
+                  min_size=matrix_col_count,
+                  max_size=matrix_col_count),
+            min_size=matrix_row_count,
+            max_size=matrix_row_count
+        ))
+
+    kernel_row_count = draw(integers(
+        min_value=1,
+        max_value=matrix_row_count,
+    ))
+    kernel_col_count = draw(integers(
+        min_value=1,
+        max_value=matrix_col_count,
+    ))
+    kernel = draw(
+        lists(
+            lists(values,
+                  min_size=kernel_col_count,
+                  max_size=kernel_col_count),
+            min_size=kernel_row_count,
+            max_size=kernel_row_count
+        ))
+
+    return (matrix, kernel)
+
+
+@given(random_matrix_and_kernel(min_dim=1, max_dim=10))
+def test_dimension_fuzz_test(matrix_and_kernel):
+    # This test merely runs the convolve on matrices and kernels of varying
+    # dimensions, to check whether the dimension calculations are off.
+    matrix, kernel = matrix_and_kernel
+    result = convolve(matrix, kernel)
+    assert len(result) == len(matrix) - len(kernel) + 1
+    assert len(result[0]) == len(matrix[0]) - len(kernel[0]) + 1
+
+
+def test_detect_edges_simple_vert():
+    matrix = [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+    ]
+    expected = [
+        [0, 4],
+        [0, 4],
+    ]
+    assert detect_edges(matrix) == expected
+
+def test_detect_edges_simple_horizontal():
+    matrix = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+    ]
+    expected = [
+        [4, 4],
+        [0, 0],
+    ]
+    assert detect_edges(matrix) == expected
