d9a16df12bf5d89c1d6f829e986b2160ea9d8f9e
Jeremy Kun <j2kun@users.noreply.github.com>
2022-02-26 14:38:07 -0800

    skill_ranking_teams: replace with new impl

--- tips/skill_ranking_teams.py
+++ tips/skill_ranking_teams.py
@@ -1,250 +1,172 @@
-'''A TrueSkill-like team-based skill ranking algorithm.'''
+'''A bare-bones implementation of two-team TrueSkill.'''
 
 from dataclasses import dataclass
-from functools import reduce
-import math
+from math import sqrt
+from statistics import NormalDist
 from typing import Dict
-from typing import Iterable
-from typing import List
 from typing import NewType
-from typing import Optional
-from typing import Union
 
-DEFAULT_STD_DEV = 25 / 3
-DEFAULT_VARIANCE = DEFAULT_STD_DEV ** 2
+STANDARD_NORMAL = NormalDist(0, 1)
 DEFAULT_MEAN = 25
+DEFAULT_STD_DEV = DEFAULT_MEAN / 3
+DEFAULT_VARIANCE = DEFAULT_STD_DEV ** 2
 
 SKILL_CLASS_WIDTH = (DEFAULT_STD_DEV / 2) ** 2
-ADDITIVE_DYNAMICS_FACTOR = (DEFAULT_STD_DEV / 100) ** 2
 DRAW_PROBABILITY = 0.01  # 1 percent
+ADDITIVE_DYNAMICS_FACTOR = (DEFAULT_STD_DEV / 100) ** 2
+TOLERANCE = 1e-18
+
+
+# players are globally unique integer ids
+Player = NewType('Player', int)
 
 
 @dataclass(frozen=True)
-class Gaussian:
-    '''A class representing a Gaussian distribution.
-
-    Instead of using the mean and variance of the Gaussian, TrueSkill uses the
-    precision and "precision-adjusted mean." These can be translated back to
-    the mean and variance easily, and the benefit is that a product or division
-    of two Gaussians corresponds to simpler arithmetic operations on precision
-    and precision-adjusted mean than the same operations would require for mean
-    and variance.
-    '''
-    precision: float = 1 / DEFAULT_VARIANCE
-    precision_adjusted_mean: float = DEFAULT_MEAN / DEFAULT_VARIANCE
-
-    @staticmethod
-    def from_mean_variance(mean, variance):
-        return Gaussian(precision=1 / variance,
-                        precision_adjusted_mean=mean / variance)
-
-    def __mul__(self, other: "Gaussian") -> "Gaussian":
-        return Gaussian(
-            precision=self.precision + other.precision,
-            precision_adjusted_mean=(
-                self.precision_adjusted_mean + other.precision_adjusted_mean))
-
-    def __truediv__(self, other: "Gaussian") -> "Gaussian":
-        return Gaussian(
-            precision=self.precision - other.precision,
-            precision_adjusted_mean=(
-                self.precision_adjusted_mean - other.precision_adjusted_mean))
-
-    def mean(self) -> float:
-        return self.precision_adjusted_mean / self.precision
-
-    def stddev(self) -> float:
-        return math.sqrt(1 / self.precision)
-
-    def __repr__(self):
-        return f'N({self.mean():.2f}, {self.stddev():.2f})'
+class Rating:
+    mean: float = DEFAULT_MEAN
+    stddev: float = DEFAULT_STD_DEV
 
 
 @dataclass(frozen=True)
-class CumulativeGaussian:
-    '''A class representing a cumulative Gaussian distribution.'''
-    mean: float
-    stddev: float
-
-    def approx_mul(self, other: "Gaussian") -> "Gaussian":
-        '''Approximate the product of a cumulative Gaussian and a Gaussian as a Gaussian.'''
-        # TODO: finish
-        return Gaussian.from_mean_variance(self.mean, self.stddev**2)
-
-    def __repr__(self):
-        return f'CumGauss(x - {self.mean:.2f}/ ({self.stddev:.2f} sqrt(2)))'
-
-
-Message = Union[Gaussian, int]
-
-
-def product(gs: Iterable[Gaussian]) -> Gaussian:
-    return reduce(lambda g1, g2: g1 * g2, gs)
-
-
-class Node:
-    '''A node of the factor graph.'''
-
-    def __init__(self):
-        self.outgoing_messages: Dict[Node, Message] = {}
-
-    def outgoing_message(self, destination: "Node", context: Optional[Gaussian] = None):
-        if destination in self.outgoing_messages:
-            msg = self.outgoing_messages[destination]
-            print(f"(cached) Message from {self} to {destination}: {msg}")
-            return msg
-        print(f"Computing new message from {self} to {destination}...")
-        msg = self.compute_outgoing_message(destination, context)
-        self.outgoing_messages[destination] = msg
-        print(f"Message from {self} to {destination}: {msg}")
-        return msg
-
-    def compute_outgoing_message(self, destination: "Node", context: Optional[Gaussian]) -> Message:
-        raise NotImplementedError()
-
-
-class VariableNode(Node):
-    '''Nodes from one half of the bipartite factor graph, corresponding to variables.'''
-
-    def __init__(self, name: str, observed_value: Optional[int] = None):
-        super().__init__()
-        self.name = name
-        self.observed_value = observed_value
-        self.factors: List[Node] = list()
-
-    def add_factor(self, factor: Node) -> None:
-        self.factors.append(factor)
-
-    def compute_outgoing_message(
-            self, destination: Node, context: Optional[Gaussian]) -> Message:
-        if self.observed_value:
-            return self.observed_value
-        return product(
-            factor.outgoing_message(self, context=None)
-            for factor in self.factors if factor != destination
-        )
-
-    def marginal_distribution(self) -> Gaussian:
-        return product(
-            factor.outgoing_message(self, context=None)
-            for factor in self.factors
-        )
-
-    def __repr__(self) -> str:
-        return self.name
-
-
-class PriorFactor(Node):
-    '''A node in the factor graph for a prior on a variable.'''
-
-    def __init__(self, variable: VariableNode, prior: Gaussian):
-        super().__init__()
-        self.variable = variable
-        self.prior = prior
-
-    def compute_outgoing_message(
-            self, destination: Node, context: Optional[Gaussian]) -> Message:
-        if destination != self.variable:
-            raise ValueError(f"Prior only sends messages to {self.variable.name}")
-        return self.prior
-
-    def __repr__(self) -> str:
-        return f"Prior_{self.prior}({self.variable.name})"
-
-
-class GaussianFactor(Node):
-    '''A node in the factor graph for a Gaussian with a variable mean.'''
-
-    def __init__(self, mean_node: VariableNode, output_node: VariableNode, variance: float):
-        super().__init__()
-        self.mean_node = mean_node
-        self.output_node = output_node
-        self.variance = variance
-
-    def compute_outgoing_message(
-            self, destination: Node, context: Optional[Gaussian]) -> Message:
-        del context  # unused
-        other_node = self.output_node if destination == self.mean_node else self.mean_node
-        msg = other_node.outgoing_message(self)
-        return Gaussian.from_mean_variance(mean=msg.mean(), variance=msg.variance() + self.variance)
-
-    def __repr__(self) -> str:
-        return f"GaussianFactor(mean={self.mean_node.name})"
-
-
-@dataclass
-class ComparisonFactor(Node):
-    '''A node in the factor graph for a left > right comparison.'''
-
-    def __init__(self, left: VariableNode, right: VariableNode, output: VariableNode):
-        super().__init__()
-        self.left = left
-        self.right = right
-        self.output = output
-
-    def compute_outgoing_message(
-            self, destination: Node, context: Optional[Gaussian]) -> Message:
-        integrate_over = self.left if destination == self.right else self.right
-        message_to_integrate = integrate_over.outgoing_message(self)
-        output_obs = self.output.outgoing_message(self)
-        if output_obs > 0 and destination == self.left:
-            message = CumulativeGaussian(
-                mean=message_to_integrate.mean(),
-                stddev=message_to_integrate.stddev(),
-            )
-
-        approximate_message = message
-        return approximate_message
-
-    def __repr__(self) -> str:
-        return f"ComparisonFactor({self.output.name} = {self.left.name} > {self.right.name})"
-
-
-def two_player_update(p1_prior: Gaussian, p2_prior: Gaussian, outcome: int):
-    '''A two-player skill update without draws, via factor graphs.'''
-
-    # construct the factor graph
-    p1_skill = VariableNode("p1_skill")
-    p2_skill = VariableNode("p2_skill")
-    observed = VariableNode("outcome", observed_value=outcome)
-
-    p1_skill_prior = PriorFactor(variable=p1_skill, prior=Gaussian())
-    p2_skill_prior = PriorFactor(variable=p2_skill, prior=Gaussian())
-    p1_skill.add_factor(p1_skill_prior)
-    p2_skill.add_factor(p2_skill_prior)
-
-    p1_perf = VariableNode("p1_perf")
-    p2_perf = VariableNode("p1_perf")
-
-    # the mean is set by a dependent variable, variance is constant
-    p1_perf_factor = GaussianFactor(
-        mean_node=p1_skill, output_node=p1_perf, variance=SKILL_CLASS_WIDTH)
-    p2_perf_factor = GaussianFactor(
-        mean_node=p2_skill, output_node=p2_perf, variance=SKILL_CLASS_WIDTH)
-
-    p1_skill.add_factor(p1_perf_factor)
-    p2_skill.add_factor(p2_perf_factor)
-    p1_perf.add_factor(p1_perf_factor)
-    p2_perf.add_factor(p2_perf_factor)
-
-    comparison_factor = ComparisonFactor(left=p1_perf, right=p2_perf, output=observed)
-    p1_perf.add_factor(comparison_factor)
-    p2_perf.add_factor(comparison_factor)
-    observed.add_factor(comparison_factor)
-
-    p1_new_prior = p1_skill.marginal_distribution()
-    p2_new_prior = p2_skill.marginal_distribution()
-
-    return p1_new_prior, p2_new_prior
-
-
-@dataclass
 class Team:
-    members: List[int]
+    # A team may have one or more players
+    ratings: Dict[Player, Rating]
 
 
-PlayerRatings = NewType("PlayerRatings", Dict[int, Gaussian])
+def compute_draw_margin() -> float:
+    '''
+      The margin to use to consider the game a draw, based on the (pre-set)
+      probability of a draw, which is typically set by measuring the draw rates
+      of a large number of games. Derived by inverting the formula
+
+        P(draw) = -1 + 2 * normal_cdf(
+            draw_margin / sqrt(player_count * skill_class_width)
+        )
+
+      or, as written exactly in the paper,
+
+        P(draw) = -1 + 2 * normal_cdf(
+            draw_margin / (sqrt(n1 + n2) * beta)
+        )
+    '''
+    inv_cdf_arg = 0.5 * (DRAW_PROBABILITY + 1)
+    inv_cdf_output = STANDARD_NORMAL.inv_cdf(inv_cdf_arg)
+    return inv_cdf_output * sqrt(2 * SKILL_CLASS_WIDTH)
 
 
-def update_ratings(team1: Team, team2: Team, ratings: PlayerRatings, outcome: int) -> PlayerRatings:
-    pass
+def truncated_onesided_gaussian_v(t: float, lower: float) -> float:
+    '''Equation 4.7 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the mean of a rectified
+    Gaussian that is only truncated from below.
+
+    This function grows roughly like lower - t for t < lower and quickly
+    approaches zero for t > lower.
+    '''
+    normalization = STANDARD_NORMAL.cdf(t - lower)
+    if normalization < TOLERANCE:
+        return lower - t   # approaches infinity
+    return STANDARD_NORMAL.pdf(t - lower) / normalization
+
+
+def truncated_onesided_gaussian_w(t: float, lower: float) -> float:
+    '''Equation 4.8 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the variance of a rectified
+    Gaussian that is only truncated from below. This is a smooth approximation
+    to an indicator function for the condition t <= lower.
+    '''
+    v_value = truncated_onesided_gaussian_v(t, lower)
+    return v_value * (v_value + t - lower)
+
+
+def truncated_twosided_gaussian_v(t, lower, upper):
+    '''Equation 4.4 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the mean of a rectified
+    Gaussian that is truncated on both sides.
+    '''
+    normalization = (STANDARD_NORMAL.cdf(upper - t) - STANDARD_NORMAL.cdf(lower - t))
+    if normalization < TOLERANCE:
+        # the limit as upper -> lower
+        return lower - t
+
+    return (
+        STANDARD_NORMAL.pdf(lower - t) - STANDARD_NORMAL.pdf(upper - t)
+    ) / normalization
+
+
+def truncated_twosided_gaussian_w(perf_diff, draw_margin):
+    '''Equation 4.4 from Herbrich '05, On Gaussian Expectation Propagation.
+
+    Representing the additive correction factor to the variance of a rectified
+    Gaussian that is truncated on both sides.
+    '''
+    abs_diff = abs(perf_diff)
+    normalization = (
+        STANDARD_NORMAL.cdf(draw_margin - abs_diff) - STANDARD_NORMAL.cdf(-draw_margin - abs_diff)
+    )
+    if normalization < TOLERANCE:
+        return 1
+
+    v_value = truncated_twosided_gaussian_v(
+        perf_diff, -draw_margin, draw_margin)
+    t1 = (draw_margin - abs_diff) * STANDARD_NORMAL.pdf(draw_margin - abs_diff)
+    t2 = (-draw_margin - abs_diff) * \
+        STANDARD_NORMAL.pdf(-draw_margin - abs_diff)
+    return v_value ** 2 + (t1 - t2) / normalization
+
+
+def update_one_team(
+        team1: Team, team2: Team, outcome: int) -> Dict[Player, Rating]:
+    '''Return the new ratings for team1.'''
+    # Each team is treated as if it were a player whose skill is the sum of the
+    # skills of individual teammates, and whose variance is the sum of
+    # variances of individual teammates. The normalization is adjusted based on
+    # the number of players.
+    draw_margin = compute_draw_margin()
+    player_count = len(team1.ratings) + len(team2.ratings)
+    t1_mean = sum(p.mean for p in team1.ratings.values())
+    t2_mean = sum(p.mean for p in team2.ratings.values())
+    t1_variance = sum(p.stddev ** 2 for p in team1.ratings.values())
+    t2_variance = sum(p.stddev ** 2 for p in team2.ratings.values())
+
+    c = sqrt(t1_variance + t2_variance + player_count * SKILL_CLASS_WIDTH)
+    winning_mean = t1_mean if outcome >= 0 else t2_mean
+    losing_mean = t2_mean if outcome >= 0 else t1_mean
+    perf_diff = winning_mean - losing_mean
+
+    if outcome == 0:
+        v = truncated_twosided_gaussian_v(
+            perf_diff / c, -draw_margin / c, draw_margin / c)
+        w = truncated_twosided_gaussian_w(perf_diff / c, draw_margin / c)
+        mean_adjustment_direction = 1
+    else:
+        v = truncated_onesided_gaussian_v(perf_diff / c, draw_margin / c)
+        w = truncated_onesided_gaussian_w(perf_diff / c, draw_margin / c)
+        mean_adjustment_direction = outcome
+
+    # Here we propagate the rating adjustment data from the team-wide summed
+    # skills down to each player. The normalization constant c is scaled up
+    # according to the sum of variances and player counts, which impacts both
+    # the value of c and the size of the multiplier that attributes team
+    # performance back to the individual player.
+    new_ratings: Dict[Player, Rating] = dict()
+    for player, rating in team1.ratings.items():
+        mean, stddev = rating.mean, rating.stddev
+        mean_multiplier = (mean ** 2 + ADDITIVE_DYNAMICS_FACTOR) / c
+        variance_plus_dynamics = stddev ** 2 + ADDITIVE_DYNAMICS_FACTOR
+        stddev_multiplier = variance_plus_dynamics / (c ** 2)
+        new_mean = mean + mean_adjustment_direction * mean_multiplier * v
+        new_stddev = sqrt(variance_plus_dynamics * (1 - w * stddev_multiplier))
+        new_ratings[player] = Rating(mean=new_mean, stddev=new_stddev)
+
+    return new_ratings
+
+
+def update_ratings(team1: Team, team2: Team, outcome: int = 1) -> Dict[Player, Rating]:
+    # Nb: in Python3.9 the bitwise-or operator is overloaded for dictionaries
+    # to perform a union of the key-value pairs. See PEP584.
+    return (
+        update_one_team(team1, team2, outcome) | update_one_team(team2, team1, -outcome)
+    )
--- tips/skill_ranking_teams_test.py
+++ tips/skill_ranking_teams_test.py
@@ -1,15 +1,147 @@
-from collections import Counter
-from statistics import NormalDist
+from skill_ranking_teams import Player
+from skill_ranking_teams import Rating
+from skill_ranking_teams import Team
+from skill_ranking_teams import update_ratings
 
-from skill_ranking_teams import Gaussian
-from skill_ranking_teams import two_player_update
+p1 = Player(1)
+p2 = Player(2)
+p3 = Player(3)
+p4 = Player(4)
 
 
-def test_two_player_update():
-    p1_prior = Gaussian()
-    p2_prior = Gaussian()
+def test_unsurprising_t1_win():
+    p1_rating = Rating(mean=10, stddev=1)
+    p2_rating = Rating(mean=11, stddev=1)
+    p3_rating = Rating(mean=2, stddev=1)
+    p4_rating = Rating(mean=3, stddev=1)
+
+    team1 = Team(ratings={
+        p1: p1_rating,
+        p2: p2_rating,
+    })
+    team2 = Team(ratings={
+        p3: p3_rating,
+        p4: p4_rating,
+    })
+
     outcome = 1
+    new_ratings = update_ratings(team1, team2, outcome)
 
-    new_p1, new_p2 = two_player_update(p1_prior, p2_prior, outcome)
-    assert new_p1.mean() > p1_prior.mean()
-    assert new_p2.mean() < p2_prior.mean()
+    new_p1_rating = new_ratings[p1]
+    new_p2_rating = new_ratings[p2]
+    new_p3_rating = new_ratings[p3]
+    new_p4_rating = new_ratings[p4]
+
+    assert new_p1_rating.mean > p1_rating.mean
+    assert new_p2_rating.mean > p2_rating.mean
+    assert new_p3_rating.mean < p3_rating.mean
+    assert new_p4_rating.mean < p4_rating.mean
+
+
+def test_surprising_t2_win():
+    p1_rating = Rating(mean=10, stddev=0.1)
+    p2_rating = Rating(mean=11, stddev=0.1)
+    p3_rating = Rating(mean=2, stddev=0.1)
+    p4_rating = Rating(mean=3, stddev=0.1)
+
+    team1 = Team(ratings={
+        p1: p1_rating,
+        p2: p2_rating,
+    })
+    team2 = Team(ratings={
+        p3: p3_rating,
+        p4: p4_rating,
+    })
+
+    outcome = -1
+    new_ratings = update_ratings(team1, team2, outcome)
+
+    new_p1_rating = new_ratings[p1]
+    new_p2_rating = new_ratings[p2]
+    new_p3_rating = new_ratings[p3]
+    new_p4_rating = new_ratings[p4]
+
+    assert new_p1_rating.mean < p1_rating.mean
+    assert new_p2_rating.mean < p2_rating.mean
+    assert new_p3_rating.mean > p3_rating.mean
+    assert new_p4_rating.mean > p4_rating.mean
+    assert new_p1_rating.stddev > p1_rating.stddev
+    assert new_p2_rating.stddev > p2_rating.stddev
+    assert new_p3_rating.stddev > p3_rating.stddev
+    assert new_p4_rating.stddev > p4_rating.stddev
+    assert (
+        new_p1_rating.mean + new_p2_rating.mean < p3_rating.mean + p4_rating.mean
+    )
+
+
+def test_team_asymmetry():
+    p1_rating = Rating(mean=10, stddev=0.1)
+    p2_rating = Rating(mean=6, stddev=0.1)
+    p3_rating = Rating(mean=2, stddev=0.1)
+    p4_rating = Rating(mean=3, stddev=0.1)
+
+    team1 = Team(ratings={
+        p1: p1_rating,
+    })
+    team2 = Team(ratings={
+        p2: p2_rating,
+        p3: p3_rating,
+        p4: p4_rating,
+    })
+
+    outcome = -1
+    new_ratings = update_ratings(team1, team2, outcome)
+
+    # team2 is just barely better than team1, so the win is not surprising.
+
+    new_p1_rating = new_ratings[p1]
+    new_p2_rating = new_ratings[p2]
+    new_p3_rating = new_ratings[p3]
+    new_p4_rating = new_ratings[p4]
+
+    assert new_p1_rating.mean < p1_rating.mean
+    assert new_p2_rating.mean > p2_rating.mean
+    assert new_p3_rating.mean > p3_rating.mean
+    assert new_p4_rating.mean > p4_rating.mean
+
+    # p3's rating goes up more due to having a bigger share of the total
+    # prior skill contribution to the team
+    assert (new_p2_rating.mean - p2_rating.mean > new_p3_rating.mean - p3_rating.mean)
+    assert (new_p2_rating.mean - p2_rating.mean > new_p4_rating.mean - p4_rating.mean)
+
+
+def test_team_asymmetry_draw():
+    p1_rating = Rating(mean=10, stddev=0.1)
+    p2_rating = Rating(mean=6, stddev=0.1)
+    p3_rating = Rating(mean=2, stddev=0.1)
+    p4_rating = Rating(mean=3, stddev=0.1)
+
+    team1 = Team(ratings={
+        p1: p1_rating,
+    })
+    team2 = Team(ratings={
+        p2: p2_rating,
+        p3: p3_rating,
+        p4: p4_rating,
+    })
+
+    outcome = 0
+    new_ratings = update_ratings(team1, team2, outcome)
+
+    # team2 is just barely better than team1, so the draw is evidence p1 is
+    # better.
+
+    new_p1_rating = new_ratings[p1]
+    new_p2_rating = new_ratings[p2]
+    new_p3_rating = new_ratings[p3]
+    new_p4_rating = new_ratings[p4]
+
+    assert new_p1_rating.mean > p1_rating.mean
+    assert new_p2_rating.mean < p2_rating.mean
+    assert new_p3_rating.mean < p3_rating.mean
+    assert new_p4_rating.mean < p4_rating.mean
+
+    # p3's rating goes down more due to having a bigger share of the total
+    # prior skill contribution to the team
+    assert (abs(new_p2_rating.mean - p2_rating.mean) > abs(new_p3_rating.mean - p3_rating.mean))
+    assert (abs(new_p2_rating.mean - p2_rating.mean) > abs(new_p4_rating.mean - p4_rating.mean))
