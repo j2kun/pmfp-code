16aeec04bcfa2d1063261eaa606ff9bffb15c7bd
Jeremy Kun <j2kun@users.noreply.github.com>
2022-03-27 14:48:35 -0700

    Add code for tip on Randomized Response (#31)
    * randomized_response: add first draft of code
    
    * randomized_response: simplify variance

---
+++ tips/randomized_response.py
@@ -0,0 +1,30 @@
+'''An implementation of a randomized response protocol.'''
+
+from typing import List
+from typing import Tuple
+import random
+
+
+def respond_privately(true_answer: bool) -> bool:
+    '''Respond to a survey with plausible deniability about your answer.'''
+    be_honest = random.random() < 0.5
+    random_answer = random.random() < 0.5
+    return true_answer if be_honest else random_answer
+
+
+def aggregate_responses(responses: List[bool]) -> Tuple[float, float]:
+    '''Return the estimated fraction of survey respondents that have a truthful
+    "Yes" answer to the survey question.
+
+    If p is the true fraction of respondents with a "Yes" answer, then in
+    expectation we will see (1/4)(1-p) + (3/4)p = (1/4) + p/2 "Yes" responses.
+    This function solves for p.
+    '''
+    yes_response_count = sum(responses)
+    n = len(responses)
+    mean = 2 * yes_response_count / n - 0.5
+    # Use n-1 when estimating variance, as per Bessel's correction.
+    # The second term represents the additional variance introduced
+    # by the private mechanism.
+    variance = 3 / (4 * (n - 1))
+    return (mean, variance)
---
+++ tips/randomized_response_test.py
@@ -0,0 +1,33 @@
+import math
+
+from hypothesis import given
+from hypothesis import example
+from hypothesis.strategies import integers
+
+from randomized_response import respond_privately
+from randomized_response import aggregate_responses
+
+
+@given(integers(min_value=25, max_value=1000),
+       integers(min_value=25, max_value=1000))
+@example(200, 0)
+@example(0, 200)
+@example(200, 200)
+@example(200, 50)
+@example(50, 200)
+def test_private_response(false_count, true_count):
+    n = false_count + true_count
+    true_mean = true_count / n
+    true_variance = 3 / (4 * n)
+
+    true_answers = [True] * true_count + [False] * false_count
+    responses = [respond_privately(ans) for ans in true_answers]
+    mean, claimed_variance = aggregate_responses(responses)
+
+    deviation_bound_true = 3 * math.sqrt(true_variance)
+    deviation_bound_claimed = 3 * math.sqrt(claimed_variance)
+
+    # the estimate is within 2 standard deviations both using the claimed
+    # variance and the true variance
+    assert abs(mean - true_mean) < deviation_bound_true
+    assert abs(mean - true_mean) < deviation_bound_claimed
