8963cbcef172791729662e09785a87f0d8736403
Jeremy Kun <j2kun@users.noreply.github.com>
2022-04-17 06:41:43 -0700

    differential_privacy: add two DP property tests and get them to pass

--- tips/differential_privacy_test.py
+++ tips/differential_privacy_test.py
@@ -16,6 +16,44 @@ class NumpyLaplaceGenerator(LaplaceGenerator):
         return np.random.default_rng().laplace(0, scale, 1)[0]
 
 
+def distributions_are_close(hist1, hist2, L2_tolerance):
+    '''
+     Decides whether two sets of random samples were likely drawn from similar
+     discrete distributions.
+
+     The distributions are considered similar if the l2 distance between them
+     is less than half the specified l2 tolerance t. Otherwise, if the distance
+     is greater than t, they are considered dissimilar. The error probability
+     is at most 4014 / (n * t^2), where n is the number of samples contained in
+     one of the sets. See https://arxiv.org/abs/1611.03579 for more
+     information, "Collision-based Testers are Optimal for Uniformity and
+     Closeness," by Ilias Diakonikolas, Themis Gouleakis, John Peebles, and
+     Eric Price.
+
+    Arguments:
+      - hist1: a dictionary whose keys are sample outcomes and whose values are
+          the frequency of the outcome in the sample, for one of the two
+          distributions being compared.
+      - hist2: same as hist1, but for a sample from the other distribution.
+      - L2_tolerance: the L2 distance between the underlying distributions, above which
+          the distributions are considered dissimilar. Lower tolerance requires
+          more samples to match the increased likelihood of error (even when
+          given two samples from the same distribution).
+
+    Returns True if the underlying distributions are within the given L2 distance.
+    '''
+    n = sum(hist1.values())
+    assert(n) == sum(hist2.values())
+
+    self_collision_1 = sum(count * (count - 1) / 2 for count in hist1.values())
+    self_collision_2 = sum(count * (count - 1) / 2 for count in hist2.values())
+    cross_collision_count = sum(hist1[sample] * hist2.get(sample, 0) for sample in hist1.keys())
+
+    test_value = (self_collision_1 + self_collision_2 - ((n - 1) / n) * cross_collision_count)
+    threshold = (L2_tolerance * (n - 1)) * (L2_tolerance * n) / 4.0
+    return test_value < threshold
+
+
 @st.composite
 def neighboring_histograms(draw, elements, min_size=1, max_size=10):
     hist = draw(st.lists(elements, min_size=min_size, max_size=max_size))
@@ -29,13 +67,30 @@ def neighboring_histograms(draw, elements, min_size=1, max_size=10):
     return (hist1, hist2)
 
 
+def test_privatize_single_number():
+    number, privacy_parameter = 17, 0.5
+
+    rng = NumpyLaplaceGenerator()
+    sample_size = 100000
+
+    def sample(num):
+        return Counter(
+            [privatize_histogram([num], privacy_parameter, rng)[0]
+             for _ in range(sample_size)])
+
+    sample_outputs = sample(number)
+    baseline = Counter([max(0, round(x)) for x in np.random.default_rng().laplace(
+        17, 1.0 / privacy_parameter, sample_size)])
+    assert distributions_are_close(sample_outputs, baseline, 1e-02)
+
+
 # @given(neighboring_histograms(st.integers(min_value=0, max_value=100)),
 #        st.floats(min_value=0.001, max_value=0.5, allow_infinity=False,
 #                  allow_nan=False),
 #         )
 # @example(((1, 2, 1, 2), (1, 2, 2, 2)), 0.5)
 def test_privatize_histogram():  # neighboring_hists, privacy_parameter):
-    neighboring_hists, privacy_parameter = ((1,), (2,)), math.log(3)
+    neighboring_hists, privacy_parameter = ((17,), (18,)), 0.5
 
     rng = NumpyLaplaceGenerator()
     hist1, hist2 = neighboring_hists
@@ -49,9 +104,16 @@ def test_privatize_histogram():  # neighboring_hists, privacy_parameter):
     sample_hist1 = sample(hist1)
     sample_hist2 = sample(hist2)
 
+    plot1 = [sample_hist1.get((i,), 0) for i in range(40)]
+    plot2 = [sample_hist2.get((i,), 0) for i in range(40)]
+    print(f'{plot1}')
+    print(f'{plot2}')
+
     def dp_test_statistic(s1, s2):
+        n = sum(s1.values())
+        assert n == sum(s2.values())
         return sum(
-            max(0.0, (s1[k] - math.exp(privacy_parameter) * s2.get(k, 0)) / len(s1))
+            max(0.0, (s1[k] - math.exp(privacy_parameter) * s2.get(k, 0)) / n)
             for k in s1.keys()
         )
 
@@ -59,5 +121,5 @@ def test_privatize_histogram():  # neighboring_hists, privacy_parameter):
     test_stat2 = dp_test_statistic(sample_hist2, sample_hist1)
 
     tolerance = 0.0025
-    assert test_stat1 < tolerance 
+    assert test_stat1 < tolerance
     assert test_stat2 < tolerance
