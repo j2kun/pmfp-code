64a1d2875d5048270266b578240f477f8db5847c
Jeremy Kun <j2kun@users.noreply.github.com>
2022-05-14 12:50:05 -0700

    Add Differential Privacy Tip implementation (#33)
    * differential_privacy: initial impl
    
    * differential_privacy: add two DP property tests and get them to pass
    
    * differential_privacy: round float to next power of 2
    
    * differential_privacy: start with a raw port of the secure rng
    
    * add a cProfile in the __main__ block
    
    * migrate from bitstring to native struct.pack/unpack
    
    * correct rng calls in sample_gemoetric
    
    * add docstring and type hints to mechanism classes
    
    * use sensitivity param instead of hard coded 1
    
    * fix bug where I wasn't adding noise to the input value
    
    * nit: use integer division
    
    * extract differential privacy test statistic
    
    * parameterize tests over mechanisms
    
    * nit: set baseline seed and use number instead of hard coded 17
    
    * tune test to 500k samples
    
    * add test for multi-bin histograms
    
    * add a second four-bin histogram test
    
    * pass rng to insecure mechanism, re-tune sample size
    
    * coverage for sample_geometric edge case
    
    * .coveragerc: omit abstract methods
    
    * refactor add_noise to return a tuple of numbers for testability

---
+++ tips/differential_privacy.py
@@ -0,0 +1,218 @@
+'''An implementation of the Laplacian mechanism for privately releasing a
+histogram where each underlying user contributes to a single bin.'''
+
+from abc import ABC, abstractmethod
+from struct import pack
+from struct import unpack
+from typing import List
+from typing import Tuple
+import math
+
+import numpy as np
+import sys
+
+
+Histogram = List[int]
+
+EXPONENT_MASK = 0x7ff0000000000000
+MANTISSA_MASK = 0x000fffffffffffff
+EXPONENT_ONE = 0x0010000000000000
+MAX_EXPONENT_BITS = unpack('>Q', pack('>d', sys.float_info.max))[0] & EXPONENT_MASK
+
+
+def next_power_of_two(x: float) -> float:
+    assert x > 0 and x != float('inf'), (
+        f"Expecting a finite, positive number, got {x}")
+
+    bits = unpack('>Q', pack('>d', x))[0]
+
+    # For a finite positive IEEE float, x is a power of 2 if and only if its
+    # mantissa is zero.
+    if bits & MANTISSA_MASK == 0:
+        return x
+
+    exponent_bits = bits & EXPONENT_MASK
+
+    assert exponent_bits < MAX_EXPONENT_BITS, f"Expecting a number less than 2^1023, got {x}"
+
+    # Add 1 to the exponent bits to get the next power of 2, resetting mantissa
+    # to zero.
+    rounded = exponent_bits + EXPONENT_ONE
+    return unpack('>d', pack('>Q', rounded))[0]
+
+
+def sample_geometric(rng, exponent):
+    """ Returns a sample drawn from the geometric distribution of parameter p =
+    1 - e^-exponent, i.e., the number of Bernoulli trials until the first
+    success where the success probability is 1 - e^-exponent.
+    """
+
+    max_value = 1 << 64
+    # Return truncated sample in the case that the sample exceeds the max value.
+    if (rng.random() > -1.0 * math.expm1(-1.0 * exponent * max_value)):
+        return max_value
+
+    # Perform a binary search for the sample in the interval from 1 to max long. Each iteration
+    # splits the interval in two and randomly keeps either the left or the right subinterval
+    # depending on the respective probability of the sample being contained in them. The search
+    # ends once the interval only contains a single sample.
+    left = 0  # exclusive
+    right = max_value  # inclusive
+
+    while (left + 1 < right):
+        # Compute a midpoint that divides the probability mass of the current
+        # interval approximately evenly between the left and right subinterval.
+        # The resulting midpoint will be less or equal to the arithmetic mean
+        # of the interval. This reduces the expected number of iterations of
+        # the binary search compared to a search that uses the arithmetic mean
+        # as a midpoint. The speed up is more pronounced, the higher the
+        # success probability p is.
+        mid = math.ceil(
+            left - (math.log(0.5) + math.log1p(math.exp(exponent * (left - right)))) / exponent
+        )
+
+        # Ensure that mid is contained in the search interval. This is a
+        # safeguard to account for potential mathematical inaccuracies due to
+        # finite precision arithmetic.
+        mid = min(max(mid, left + 1), right - 1)
+
+        # Probability that the sample is at most mid, i.e.,
+        #
+        #    q = Pr[X ≤ mid | left < X ≤ right]
+        #
+        # where X denotes the sample. The value of q should be approximately
+        # one half.
+        q = math.expm1(exponent * (left - mid)) / math.expm1(exponent * (left - right))
+        if (rng.random() <= q):
+            right = mid
+        else:
+            left = mid
+
+    return right
+
+
+def sample_two_sided_geometric(rng, exponent):
+    """Returns a sample drawn from a geometric distribution that is mirrored at
+    0. The non-negative part of the distribution's PDF matches the PDF of a
+    geometric distribution of parameter p = 1 - e^-exponent that is
+    shifted to the left by 1 and scaled accordingly.
+    """
+    geometric_sample = 0
+    sign = False
+
+    # Keep a sample of 0 only if the sign is positive. Otherwise, the
+    # probability of 0 would be twice as high as it should be.
+    while (geometric_sample == 0 and not sign):
+        geometric_sample = sample_geometric(rng, exponent) - 1
+        sign = rng.random() < 0.5
+
+    return geometric_sample if sign else -geometric_sample
+
+
+class LaplaceMechanism(ABC):
+    '''An interface for a random number generator that adds Laplacian noise to
+    a single number, generated from a 0-mean discrete Laplacian distribution,
+    i.e., with density h(y) proportional to exp(−|y|/scale), where scale is a
+    parameter.
+
+    Because the scale and the details of the mechanism depend on the privacy
+    parameter, the method does not accept scale directly, but must instead
+    derive the appropriate scale from the privacy parameters.
+
+    Args:
+     - value: the integer value to add noise to
+     - privacy_parameter: the epsilon in differential privacy
+     - sensitivity: the maximum value a single user can influence the number
+         being masked.
+
+    Returns:
+      A pair of numbers, which when added together is a masked version of
+      `value` that satisfies epsilon differential privacy. The first argument
+      represents the `value`, which may be rounded to a multiple of a power of
+      2 for security, and the second argument represents the noise to add to
+      the first argument.
+    '''
+    @abstractmethod
+    def add_noise(self, value: int, privacy_parameter: float, sensitivity: float) -> Tuple[int, int]:
+        ...
+
+
+class InsecureLaplaceMechanism(LaplaceMechanism):
+    def __init__(self, rng=None):
+        self.rng = rng or np.random.default_rng(1)
+
+    def add_noise(self, value: int, privacy_parameter: float, sensitivity: float) -> Tuple[int, int]:
+        scale = sensitivity / privacy_parameter
+        return (value, round(self.rng.laplace(0, scale, 1)[0]))
+
+
+class SecureLaplaceMechanism(LaplaceMechanism):
+    '''A secure random generator for use in Differential Privacy.
+
+    Follows the outline of
+    https://github.com/google/differential-privacy/blob/main/common_docs/Secure_Noise_Generation.pdf
+    and the reference implementation at
+    https://github.com/google/differential-privacy/blob/c2376f0daaf406e1524b462accaa9cbb548fd6d1/java/main/com/google/privacy/differentialprivacy/LaplaceNoise.java
+
+    For simplicity, we only support adding noise to integers. Adding noise to
+    float values requires additional rounding of the input value to be a
+    multiple of the granularity.
+    '''
+    GRANULARITY_PARAM = float(1 << 40)
+
+    def __init__(self, rng):
+        self.rng = rng
+
+    def add_noise(self, value: int, privacy_parameter: float, sensitivity: float) -> Tuple[int, int]:
+        eps = privacy_parameter
+        granularity = next_power_of_two((sensitivity / eps) / self.GRANULARITY_PARAM)
+        noise = sample_two_sided_geometric(
+            self.rng, granularity * eps / (sensitivity + granularity))
+
+        # note this is where we rely on `value` being an integer, otherwise
+        # we'd need to round `value` to a multiple of granularity.
+        if granularity <= 1:
+            return (value, round(noise * granularity))
+        else:
+            # Technically this else statement is not necessary for this Tip,
+            # because the sensitivity is 1 and epsilon should not be so
+            # unreasonably small that it makes the granularity bigger than 1.
+            # However, I ported this from the reference implementation, and
+            # I'm leaving it for completeness.
+            granularity = int(granularity)
+            rounded = granularity * (
+                value // granularity + round((value % granularity) / granularity)
+            )
+            return (rounded, noise * granularity)
+
+
+def privatize_histogram(
+        hist: Histogram,
+        privacy_parameter: float,
+        laplace: LaplaceMechanism):
+    '''Privatize a histogram for public release.
+
+    This implementation relies on the following properties:
+
+     - Each user contributes at most a count of one to at most one bin.
+     - The categories (bin definitions) of the histogram are fixed and public.
+
+    If these are satisfied, then the mechanism satsifies epsilon-differential
+    privacy for epsilon = `privacy_parameter`, meaning that if any individual
+    is removed from the dataset, the probability of this mechanism producing a
+    different output is at most exp(privacy_parameter). This limits the amount
+    of information any attacker (using any method or extra side data) can learn
+    about one individual in the dataset.
+    '''
+    noisy_hist = [
+        sum(laplace.add_noise(bin_value, privacy_parameter, sensitivity=1))
+        for bin_value in hist
+    ]
+    return [max(0, val) for val in noisy_hist]
+
+
+if __name__ == "__main__":
+    import cProfile
+    cProfile.run(
+        'for i in range(100000): '
+        'privatize_histogram((17,), math.log(3), SecureLaplaceMechanism(random.SystemRandom()))')
---
+++ tips/differential_privacy_test.py
@@ -0,0 +1,176 @@
+from collections import Counter
+import math
+import random
+
+from hypothesis import strategies as st
+from hypothesis import given
+from hypothesis import example
+import numpy as np
+import pytest
+
+from differential_privacy import InsecureLaplaceMechanism
+from differential_privacy import SecureLaplaceMechanism
+from differential_privacy import next_power_of_two
+from differential_privacy import privatize_histogram
+from differential_privacy import sample_geometric
+
+
+def distributions_are_close(hist1, hist2, L2_tolerance):
+    '''
+     Decides whether two sets of random samples were likely drawn from similar
+     discrete distributions.
+
+     The distributions are considered similar if the l2 distance between them
+     is less than half the specified l2 tolerance t. Otherwise, if the distance
+     is greater than t, they are considered dissimilar. The error probability
+     is at most 4014 / (n * t^2), where n is the number of samples contained in
+     one of the sets. See https://arxiv.org/abs/1611.03579 for more
+     information, "Collision-based Testers are Optimal for Uniformity and
+     Closeness," by Ilias Diakonikolas, Themis Gouleakis, John Peebles, and
+     Eric Price.
+
+    Arguments:
+      - hist1: a dictionary whose keys are sample outcomes and whose values are
+          the frequency of the outcome in the sample, for one of the two
+          distributions being compared.
+      - hist2: same as hist1, but for a sample from the other distribution.
+      - L2_tolerance: the L2 distance between the underlying distributions, above which
+          the distributions are considered dissimilar. Lower tolerance requires
+          more samples to match the increased likelihood of error (even when
+          given two samples from the same distribution).
+
+    Returns True if the underlying distributions are within the given L2 distance.
+    '''
+    n = sum(hist1.values())
+    assert(n) == sum(hist2.values())
+
+    self_collision_1 = sum(count * (count - 1) / 2 for count in hist1.values())
+    self_collision_2 = sum(count * (count - 1) / 2 for count in hist2.values())
+    cross_collision_count = sum(hist1[sample] * hist2.get(sample, 0) for sample in hist1.keys())
+
+    test_value = (self_collision_1 + self_collision_2 - ((n - 1) / n) * cross_collision_count)
+    threshold = (L2_tolerance * (n - 1)) * (L2_tolerance * n) / 4.0
+    return test_value < threshold
+
+
+def dp_test_statistic(s1, s2, privacy_parameter):
+    '''Compute the epsilon-delta differential privacy test statistic.
+
+    This checks whether the differential privacy mechanism, which outputs
+    samples s1 and s2 from two neighboring databases, approximately satisfies
+    differential privacy. Because the code in this Tip focuses on epsilon-DP,
+    not epsilon-delta DP, but epsilon-DP is not testable in general, we resort
+    to testing for epsilon-delta DP with a very small delta, hard coded in the
+    tests that call this function.
+
+    This statistic is from Gilbert-McMillan 2018, "Property Testing for
+    Differential Privacy", Theorem 14 and Algorithm 2.
+    https://arxiv.org/abs/1806.06427
+
+    Also see a reference implementation at
+    https://github.com/google/differential-privacy/blob/c2376f0daaf406e1524b462accaa9cbb548fd6d1/java/main/com/google/privacy/differentialprivacy/testing/StatisticalTestsUtil.java#L88-L137
+    '''
+    n = sum(s1.values())
+    assert n == sum(s2.values())
+    return sum(
+        max(0.0, (s1[k] - math.exp(privacy_parameter) * s2.get(k, 0)) / n)
+        for k in s1.keys()
+    )
+
+
+def make_mechanisms():
+    mechs = [InsecureLaplaceMechanism(), SecureLaplaceMechanism(random.SystemRandom())]
+    return [(x.__class__.__name__, x) for x in mechs]
+
+
+@given(st.floats(
+    min_value=0.0, max_value=2**1023, allow_infinity=False, allow_nan=False, exclude_min=True
+))
+@example(float(2**-1023))
+@example(float(2**1023))
+def test_next_power_of_two(x):
+    output = next_power_of_two(x)
+    expected = min(float(2**i) for i in range(-1022, 1024) if 2**i >= x)
+    assert expected == output
+
+
+def test_sample_geometric_truncation():
+    assert sample_geometric(random.Random(1), 1e-100) == 1 << 64
+
+
+def test_large_granularity():
+    mechanism = SecureLaplaceMechanism(random.Random(1))
+    output = mechanism.add_noise(11, 0.3, 1 << 40)  # results in a ganularity of 4
+    # This proves that when the granularity is large, the value is first
+    # rounded to a multiple of granularity before the noise is added.
+    assert output[0] == 12
+
+
+@pytest.mark.parametrize("name,mechanism", make_mechanisms())
+def test_privatize_single_number(name, mechanism):
+    number, privacy_parameter = 17, 0.5
+    sample_size = 100000
+
+    def sample(num):
+        return Counter(
+            [privatize_histogram([num], privacy_parameter, mechanism)[0]
+             for _ in range(sample_size)])
+
+    sample_outputs = sample(number)
+    baseline = Counter([max(0, round(x)) for x in np.random.default_rng(1).laplace(
+        number, 1.0 / privacy_parameter, sample_size)])
+    assert distributions_are_close(sample_outputs, baseline, 1e-02)
+
+
+@pytest.mark.parametrize("name,mechanism", make_mechanisms())
+def test_privatize_single_bin_histogram(name, mechanism):
+    neighboring_hists, privacy_parameter = ((17,), (18,)), math.log(3)
+    hist1, hist2 = neighboring_hists
+    sample_size = 600000
+
+    def sample(hist):
+        return Counter(
+            [tuple(privatize_histogram(hist, privacy_parameter, mechanism))
+             for _ in range(sample_size)])
+
+    sample_hist1 = sample(hist1)
+    sample_hist2 = sample(hist2)
+
+    plot1 = [sample_hist1.get((i,), 0) for i in range(40)]
+    plot2 = [sample_hist2.get((i,), 0) for i in range(40)]
+    print(f'{plot1}')
+    print(f'{plot2}')
+
+    test_stat1 = dp_test_statistic(sample_hist1, sample_hist2, privacy_parameter)
+    test_stat2 = dp_test_statistic(sample_hist2, sample_hist1, privacy_parameter)
+
+    tolerance = 0.0025
+    assert test_stat1 < tolerance
+    assert test_stat2 < tolerance
+
+
+@pytest.mark.parametrize("name,mechanism", make_mechanisms())
+@pytest.mark.parametrize("neighboring_hists", [
+    ((1, 2, 1, 2), (1, 2, 2, 2)),
+    ((10, 15, 9, 14), (9, 15, 9, 14)),
+])
+def test_privatize_multi_bin_histogram(name, mechanism, neighboring_hists):
+    privacy_parameter = math.log(3)
+    mechanism = InsecureLaplaceMechanism()
+    hist1, hist2 = neighboring_hists
+    sample_size = 250000
+
+    def sample(hist):
+        return Counter(
+            [tuple(privatize_histogram(hist, privacy_parameter, mechanism))
+             for _ in range(sample_size)])
+
+    sample_hist1 = sample(hist1)
+    sample_hist2 = sample(hist2)
+
+    test_stat1 = dp_test_statistic(sample_hist1, sample_hist2, privacy_parameter)
+    test_stat2 = dp_test_statistic(sample_hist2, sample_hist1, privacy_parameter)
+
+    tolerance = 0.05
+    assert test_stat1 < tolerance
+    assert test_stat2 < tolerance
