44ba75c22ba6a2fc107c3b2c3fd22f86c00b84fc
Jeremy Kun <j2kun@users.noreply.github.com>
2022-07-09 13:11:10 -0700

    topological_sort: implement (#35)
    Adds first draft of implementation and tests
    
    Hypothesis tests for random DAGs with and without loops,
    along with a few static parameterized tests.

---
+++ tips/topological_sort.py
@@ -0,0 +1,58 @@
+"""Compute a topological sort of a directed, acyclic graph."""
+from collections import deque
+
+# The key is the node's unique id, and the value for node N is the list of node
+# ids that depend on N.
+Graph = dict[int, list[int]]
+
+
+def topological_sort(dag: Graph) -> list[int]:
+    """Compute a topological sorting of the input graph.
+
+    The edges in the graph are interpreted so that there is an edge from node i
+    to node j if node j depends on node i. An edge from node i to node j is
+    represented in the input by j being a member of dag[i].
+
+    The input graph is required to have no cycles, i.e., be a directed, acyclic
+    graph (DAG).
+
+    Arguments:
+        dag: a dictionary mapping node ids to their downstream dependencies.
+
+    Returns:
+        A list of node ids in topological sorted order, meaning upstream
+        dependencies come first in the list.
+
+    Raises:
+        KeyError: if a node id cannot be found.
+        ValueError: if the input graph contains cycles.
+    """
+    sorted_nodes: deque = deque([], maxlen=len(dag))
+    visited_overall = set()
+    visited_single_dfs = set()
+    starting_nodes = list(dag.keys())
+
+    def dfs_helper(node):
+        visited_single_dfs.add(node)
+        for dependent in dag[node]:
+            if dependent in visited_single_dfs and dependent not in visited_overall:
+                raise ValueError(
+                    f"Input graph {dag} is not a DAG! Node {dependent} is part of a loop"
+                )
+            if dependent in visited_overall:
+                continue
+            dfs_helper(dependent)
+        visited_overall.add(node)
+        sorted_nodes.appendleft(node)
+
+    # Because we select unvisited notes essentially at random, and because the
+    # graph not be a single connected component, we must admit the possibility
+    # that we need to run multiple DFS loops to find all the nodes.
+    while starting_nodes:
+        starting_node = starting_nodes.pop()
+        if starting_node in visited_overall:
+            continue
+        visited_single_dfs = set()
+        dfs_helper(starting_node)
+
+    return list(sorted_nodes)
---
+++ tips/topological_sort_test.py
@@ -0,0 +1,172 @@
+from itertools import combinations
+import random
+
+from hypothesis import given
+from hypothesis.strategies import booleans
+from hypothesis.strategies import composite
+from hypothesis.strategies import integers
+import pytest
+
+from topological_sort import topological_sort
+
+
+def assert_satisfies_dependency_order(dag, sorted_nodes):
+    # Test that a sorted output satisfies the dependency requirements of the
+    # DAG.
+    for (node, dependents) in dag.items():
+        for dependent in dependents:
+            node_index = sorted_nodes.index(node)
+            dependent_index = sorted_nodes.index(dependent)
+            assert node_index < dependent_index, sorted_nodes
+
+
+@pytest.mark.parametrize(
+    "dag,expected", [
+        (dict(), []),
+        ({
+            1: []
+        }, [1]),
+        ({
+            1: [2],
+            2: []
+        }, [1, 2]),
+        ({
+            2: [1, 3],
+            1: [],
+            3: [1]
+        }, [2, 3, 1]),
+        ({
+            0: [1],
+            1: [2],
+            2: [3],
+            3: [4],
+            4: []
+        }, [0, 1, 2, 3, 4]),
+    ]
+)
+def test_unique_toposort(dag, expected):
+    assert expected == topological_sort(dag)
+
+
+@pytest.mark.parametrize(
+    "dag", [
+        {
+            1: [2, 3],
+            2: [],
+            3: [],
+        },
+        {
+            1: [2, 3],
+            2: [4],
+            3: [4],
+            4: [],
+        },
+        {
+            1: [2, 4],
+            2: [3, 5],
+            3: [4, 6],
+            4: [5],
+            5: [],
+            6: [],
+        },
+    ]
+)
+def test_non_unique_toposort(dag):
+    assert_satisfies_dependency_order(dag, topological_sort(dag))
+
+
+@pytest.mark.parametrize("dag", [
+    {
+        1: [2],
+    },
+    {
+        1: [2],
+        2: [3],
+        3: [4],
+    },
+])
+def test_missing_keys(dag):
+    with pytest.raises(KeyError):
+        topological_sort(dag)
+
+
+@pytest.mark.parametrize(
+    "dag", [
+        {
+            1: [2],
+            2: [1],
+        },
+        {
+            1: [2],
+            2: [3],
+            3: [1],
+        },
+    ]
+)
+def test_loop_detection(dag):
+    with pytest.raises(ValueError):
+        topological_sort(dag)
+
+
+@composite
+def random_dag(
+    draw,
+    dependency_decider=booleans(),
+    num_nodes=integers(min_value=1, max_value=100),
+):
+    """Generate a random DAG."""
+    nodes = list(range(draw(num_nodes)))
+    dag = dict((i, []) for i in nodes)
+    random.shuffle(nodes)
+
+    for (i, j) in combinations(nodes, 2):
+        if draw(dependency_decider):
+            dag[i].append(j)
+
+    return dag
+
+
+@given(random_dag())
+def test_random_dag(dag):
+    assert_satisfies_dependency_order(dag, topological_sort(dag))
+
+
+@composite
+def random_dag_with_loop(
+    draw,
+    dag_builder=random_dag(),
+    loop_member_picker=booleans(),
+):
+    """Generate a random DAG, then pick a random subset of nodes and form a
+    directed loop.
+    """
+    dag = draw(dag_builder)
+
+    # the cycle must have at least one node, and the picker may always return
+    # false.
+    chosen_nodes = set([random.choice(list(dag.keys()))])
+
+    for node in dag.keys():
+        if draw(loop_member_picker):
+            chosen_nodes.add(node)
+
+    loop = list(chosen_nodes)
+    random.shuffle(loop)
+
+    for (i, j) in zip(loop, loop[1:]):
+        dependents = set(dag[i])
+        dependents.add(j)
+        dag[i] = list(dependents)
+
+    # add the last edge looping back to the first
+    dependents = set(dag[loop[-1]])
+    dependents.add(loop[0])
+    dag[loop[-1]] = list(dependents)
+
+    return dag
+
+
+@given(random_dag_with_loop())
+def test_err_on_random_dag_with_cycle(dag):
+    with pytest.raises(ValueError):
+        topological_sort(dag)
