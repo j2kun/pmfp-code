075ae352f4cd7fd42c69d84b1c8167cc8b918ce7
Jeremy Kun <j2kun@users.noreply.github.com>
2022-12-11 11:08:39 -0800

    move functions to main, reformat

--- util/generate_hamming_code.py
+++ util/generate_hamming_code.py
@@ -1,39 +1,38 @@
 import numpy
 
+# fmt: off
 P = numpy.array([
     1, 1, 0,
     1, 0, 1,
     0, 1, 1,
     1, 1, 1,
 ], dtype=numpy.uint8).reshape((4, 3))
+# fmt: on
 
 generator_matrix = numpy.concatenate(
-    (numpy.identity(n=4, dtype=numpy.uint8), P),
-    axis=1
+    (numpy.identity(n=4, dtype=numpy.uint8), P), axis=1
 )
 
 parity_check_matrix = numpy.concatenate(
-    (P.transpose(), numpy.identity(n=3, dtype=numpy.uint8)),
-    axis=1
+    (P.transpose(), numpy.identity(n=3, dtype=numpy.uint8)), axis=1
 )
 
 
-def to_vec(x, length):
-    vector = numpy.zeros((length,), dtype=numpy.uint8)
-    for bit in range(length):
-       vector[length - bit - 1] = (x & (1 << bit)) >> bit
-    return vector
-
-
-def from_vec(x):
-    n = len(x)
-    output = 0
-    for i in range(n):
-       output |= x[n - i - 1] << i
-    return output
-
-
 if __name__ == "__main__":
+
+    def to_vec(x, length):
+        vector = numpy.zeros((length,), dtype=numpy.uint8)
+        for bit in range(length):
+            vector[length - bit - 1] = (x & (1 << bit)) >> bit
+        return vector
+
+    def from_vec(x):
+        n = len(x)
+        output = 0
+        for i in range(n):
+            output |= x[n - i - 1] << i
+        return output
+
     print(generator_matrix)
     print(parity_check_matrix)
     print(parity_check_matrix.dot(generator_matrix.T) % 2)
@@ -42,19 +41,21 @@ if __name__ == "__main__":
     for i in range(16):
         input_vector = to_vec(i, 4)
         encoded = input_vector.dot(generator_matrix) % 2
-        print(f'i={i}={input_vector}, o={from_vec(encoded)}={encoded}')
+        print(f"i={i}={input_vector}, o={from_vec(encoded)}={encoded}")
         encoder_lookup.append(from_vec(encoded))
-    print(f'encoder = {encoder_lookup}')
+    print(f"encoder = {encoder_lookup}")
 
     decoder_bits = [0] * 3
     for i, row in enumerate(parity_check_matrix):
         row_as_int = from_vec(row)
         decoder_bits[i] = row_as_int
 
-    print('syndrome = ('
-          f'4 * parity(block & {decoder_bits[0]}) '
-          f'+ 2 * parity(block & {decoder_bits[1]}) '
-          f'+ parity(block & {decoder_bits[2]}))')
+    print(
+        "syndrome = ("
+        f"4 * parity(block & {decoder_bits[0]}) "
+        f"+ 2 * parity(block & {decoder_bits[1]}) "
+        f"+ parity(block & {decoder_bits[2]}))"
+    )
 
     syndrome_lookup = [0] * 8
     for i in range(7):
@@ -64,4 +65,4 @@ if __name__ == "__main__":
         syndrome = parity_check_matrix.dot(error_vector) % 2
         lookup_input = from_vec(syndrome)
         syndrome_lookup[lookup_input] = error_bit
-    print(f'syndrome_table = {syndrome_lookup}')
+    print(f"syndrome_table = {syndrome_lookup}")
