79b417720c7ad02c67b80a3e7ecb098a30ba390b
Jeremy Kun <j2kun@users.noreply.github.com>
2023-02-21 15:44:13 -0800

    gp_ucb: add initial implementation of GP UCB

--- tips/gp_ucb.py
+++ tips/gp_ucb.py
@@ -5,6 +5,7 @@ sci-kit learn implementation.
 
 https://github.com/scikit-learn/scikit-learn/blob/8c9c1f27b7e21201cfffb118934999025fd50cca/sklearn/gaussian_process/_gpr.py
 """
+import itertools
 
 from scipy.linalg import cholesky, cho_solve, solve_triangular
 from scipy.spatial.distance import cdist
@@ -21,6 +22,8 @@ class GaussianProcessRegressor:
         # Normalize training signals
         self._y_train_mean = np.mean(y, axis=0)
         self._y_train_std = np.std(y, axis=0)
+        if self._y_train_std == 0:
+            self._y_train_std = 1
         y = (y - self._y_train_mean) / self._y_train_std
 
         self.X_train_ = X
@@ -71,3 +74,49 @@ class GaussianProcessRegressor:
             y_cov = np.squeeze(y_cov, axis=2)
 
         return y_mean, y_cov
+
+
+class GpUcb:
+    def __init__(self, input_spaces, beta=10):
+        """Initalize the GP UCB algorithm."""
+        self.beta = beta
+
+        # Flatten the input space so it can be indexed by a single integer.
+        # A smarter approach would use an implicit indexing scheme.
+        self.features = input_spaces
+        self.input_space = list(itertools.product(*input_spaces))
+
+        # Initial mean/stdev values are chosen arbitrarily here,
+        # but in Vizier, input parameters have a variety of allowed
+        # configurations that impact the initial values. See
+        # https://github.com/google/vizier/blob/3e2581814f219a29c2e540c3df8d8a5c911d55ce/vizier/_src/pyvizier/shared/parameter_config.py#L238
+        self.mean = np.array([0] * len(self.input_space))
+        self.stdev = np.array([0.5] * len(self.input_space))
+
+        self.obs_inputs = []
+        self.obs_outputs = []
+
+    def suggest(self):
+        index = np.argmax(self.mean + self.stdev * self.beta**0.5)
+        suggestion = self.input_space[index]
+        self.obs_inputs.append(suggestion)
+        return suggestion
+
+    def update(self, observed_output):
+        self.obs_outputs.append(observed_output)
+        gp = GaussianProcessRegressor()
+        gp.train(self.obs_inputs, self.obs_outputs)
+        self.mean, cov = gp.predict(self.input_space)
+        self.stdev = cov.diagonal()**0.5
+
+    def get_mean(self, input):
+        # inefficient but simple
+        return self.mean[self.input_space.index(input)]
+
+    def unwind_estimates(self):
+        output = dict()
+        indices = itertools.product(*[range(len(x)) for x in self.features])
+        for flat_index, index_tuple in enumerate(indices):
+            input = tuple(f[i] for (f, i) in zip(self.features, index_tuple))
+            output[input] = (self.mean[flat_index], self.stdev[flat_index])
+        return output
