b2f629fc5a649e17949b19f9657a154060813114
Jeremy Kun <j2kun@users.noreply.github.com>
2023-09-09 20:57:05 -0700

    patrol_scheduling: initial files

---
+++ tips/patrol_scheduling.py
@@ -0,0 +1,9 @@
+"""An implementation of a patrol scheduling algorithm.
+
+Similar to what is used in the PAWS project, see https://github.com/lily-x/mirror and
+https://arxiv.org/abs/2106.08413
+"""
+
+
+def schedule_patrols():
+    return None
