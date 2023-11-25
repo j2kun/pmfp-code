a932a18f667b639de7cdaf23f1b4c88c8ed9904c
Jeremy Kun <j2kun@users.noreply.github.com>
2023-11-25 11:55:10 -0800

    add parameter selection figure

--- tips/pid.py
+++ tips/pid.py
@@ -40,3 +40,69 @@ class PrincipalIntegralDerviativeController:
         output = self.kp * error + self.integral + self.kd * derivative
         self.last_measurement = measurement
         return clamp(output, self.output_min, self.output_max)
+
+
+# Plot the PID control in a simulated environment
+if __name__ == "__main__":
+    from dataclasses import dataclass, replace
+
+    import matplotlib.pyplot as plt
+
+    @dataclass(frozen=True)
+    class SimpleLinearSystem:
+        value: float = 0
+        time: int = 0
+
+        def run(self, control: int):
+            change = control / 20
+            new_value = self.value + change
+            return replace(self, value=new_value, time=self.time + 1)
+
+    samples = 200
+    xs = list(range(samples))
+    dt = 0.1
+    initial = 0
+    setpoint = 10
+    PID = PrincipalIntegralDerviativeController
+
+    pids = [
+        PID(kp=5.0, ki=0.5, kd=0.5, setpoint=setpoint, output_min=-100, output_max=100),
+        PID(kp=1.0, ki=1.0, kd=0.5, setpoint=setpoint, output_min=-100, output_max=100),
+        PID(kp=0.3, ki=0.0, kd=0.5, setpoint=setpoint, output_min=-100, output_max=100),
+    ]
+    styles = [
+        "dashed",
+        "dotted",
+        "dashdot",
+    ]
+    plt.subplots(figsize=(8, 4))
+    plt.axhline(
+        y=setpoint,
+        color="black",
+        linestyle="solid",
+        label="setpoint",
+        linewidth=1,
+    )
+
+    for pid, style in zip(pids, styles):
+        system = SimpleLinearSystem(value=initial)
+        control_values = []
+        system_values = []
+        for i in range(samples):
+            control = pid.run(system.value, dt=dt)
+            system = system.run(control)
+            control_values.append(control)
+            system_values.append(system.value)
+
+        plt.plot(
+            xs,
+            system_values,
+            linewidth=2,
+            label=f"kp={pid.kp:G}, ki={pid.ki:G}, kd={pid.kd:G}",
+            linestyle=style,
+        )
+
+    plt.xlabel("timestep")
+    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    plt.tight_layout()
+    plt.show()
