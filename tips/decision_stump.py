3c27c22018eff966aca40bf15ea277906935cb7a
Jeremy Kun <j2kun@users.noreply.github.com>
2025-01-02 22:59:58 -0800

    remove uncovered lines that aren't used

--- tips/decision_stump.py
+++ tips/decision_stump.py
@@ -28,8 +28,7 @@ class DecisionStump:
 
 def most_common_label(data: Dataset) -> int:
     """Compute the label that occurs most commonly in the set of labeled examples."""
-    if not data:
-        return -1
+    assert data
     return Counter([label for (_, label) in data]).most_common(1)[0][0]
 
 
