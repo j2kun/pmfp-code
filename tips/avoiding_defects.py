f832c008aa06540d5e311189ea8fc6c45d9d93c9
Jeremy Kun <j2kun@users.noreply.github.com>
2025-01-25 22:54:40 -0800

    first pass at implementing sampling scheme finder

--- tips/avoiding_defects.py
+++ tips/avoiding_defects.py
@@ -1,2 +1,87 @@
+from typing import Iterable, Union
+
+from scipy.stats import hypergeom
+
+
+def calculate_oc_hypergeom(
+    sample_size: int,
+    acceptance_number: int,
+    population_size: int,
+    defective_count: Union[int, Iterable[int]],
+) -> Union[float, list[float]]:
+    """Calculate the Operating Characteristic (OC) probability for a single-stage
+    hypergeometric sampling plan.
+
+    Returns float or list of floats computing the probability of accepting the lot given
+    the sampling plan.
+    """
+    if not hasattr(defective_count, "__iter__"):
+        defective_count = [defective_count]
+
+    p_accept = [
+        hypergeom.cdf(
+            k=acceptance_number,  # accept if we find <= c defects in the sample
+            M=population_size,  # population size
+            n=sample_size,  # sample size
+            N=defects,  # number of defects in the population
+        )
+        for defects in defective_count
+    ]
+
+    # If input was single number, return single number
+    if len(p_accept) == 1:
+        return p_accept[0]
+    return p_accept
+
+
+def find_plan_hypergeom(
+    producer_risk_point: tuple[float, float],
+    consumer_risk_point: tuple[float, float],
+    population_size: int,
+) -> tuple[int, int]:
+    """Find the best single-scheme sampling plan.
+
+    Args:
+        producer_risk_point: (d, p) implying lots with defect rate <= d are
+            accepted with probability >= p
+        consumer_risk_point: (d, p) implying lots with defect rate >= d are
+            accepted with probability <= p
+        population_size: Size of the lot
+
+    Returns: a tuple containing the best sample size and number of defects allowed.
+    """
+    assert 0 < producer_risk_point[0] < 1
+    assert 0 < producer_risk_point[1] < 1
+    assert 0 < consumer_risk_point[0] < 1
+    assert 0 < consumer_risk_point[1] < 1
+
+    defects_accepted = 0
+    sample_size = defects_accepted + 1
+
+    while True:
+        prob_accept_bad_lot = calculate_oc_hypergeom(
+            sample_size=sample_size,
+            acceptance_number=defects_accepted,
+            population_size=population_size,
+            defective_count=consumer_risk_point[0] * population_size,
+        )
+        prob_accept_good_lot = calculate_oc_hypergeom(
+            sample_size=sample_size,
+            acceptance_number=defects_accepted,
+            population_size=population_size,
+            defective_count=producer_risk_point[0] * population_size,
+        )
+
+        if prob_accept_bad_lot > consumer_risk_point[1]:
+            sample_size += 1  # Increase sample size to be more stringent
+        elif prob_accept_good_lot < producer_risk_point[1]:
+            defects_accepted += 1  # Allow one more defect to be more lenient
+        else:
+            # If both checks pass, the plan is good
+            break
+
+    return (sample_size, defects_accepted)
+
+
 def single_sampling_scheme():
     pass
