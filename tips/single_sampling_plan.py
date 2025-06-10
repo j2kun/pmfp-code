7c226d13f5d8370716f62e097818a0f6b6077f7c
Jeremy Kun <kun.jeremy@gmail.com>
2025-06-09 22:04:24 -0700

    Tip for single sampling scheme (#85)
    * add template files
    
    * first pass at implementing sampling scheme finder
    
    * mypy
    
    * finish single sampling scheme
    
    * add degenerate
    
    * convert OC curve function to main plt
    
    * make terminology more consistent, use dataclass over tuple
    
    * improve charting
    
    * extract graphing into a function
    
    * finish draft of tip
    
    * rename files more sensibly
    
    ---------
    
    Co-authored-by: Jeremy Kun <j2kun@users.noreply.github.com>

---
+++ tips/single_sampling_plan.py
@@ -0,0 +1,176 @@
+import random
+from dataclasses import dataclass
+
+import scipy.stats
+
+
+@dataclass
+class SingleSamplingPlan:
+    # The number of items to sample from the lot
+    sample_size: int
+
+    # The maximum number of defective items in the sample before the lot is
+    # rejected
+    acceptance_number: int
+
+
+def hypergeom(
+    sample_size: int,
+    acceptance_number: int,
+    population_size: int,
+    defective_count: int,
+) -> float:
+    """Calculate the hypergeometric function as used by the single sampling plan."""
+    return scipy.stats.hypergeom.cdf(
+        k=acceptance_number,  # accept if we find <= c defects in the sample
+        M=population_size,  # population size
+        n=sample_size,  # sample size
+        N=defective_count,  # number of defects in the population
+    )
+
+
+def find_plan_hypergeom(
+    producer_risk_point: tuple[float, float],
+    consumer_risk_point: tuple[float, float],
+    population_size: int,
+) -> SingleSamplingPlan:
+    """Find the best single sampling plan.
+
+    Args:
+        producer_risk_point: (d, p) implying lots with defect rate <= d are
+            accepted with probability >= p
+        consumer_risk_point: (d, p) implying lots with defect rate >= d are
+            accepted with probability <= p
+        population_size: Size of the lot
+
+    Returns: a tuple containing the best sample size and number of defects allowed.
+    """
+    assert 0 < producer_risk_point[0] < 1
+    assert 0 < producer_risk_point[1] < 1
+    assert 0 < consumer_risk_point[0] < 1
+    assert 0 < consumer_risk_point[1] < 1
+
+    defects_accepted = 0
+    sample_size = defects_accepted + 1
+
+    while True:
+        prob_accept_bad_lot = hypergeom(
+            sample_size=sample_size,
+            acceptance_number=defects_accepted,
+            population_size=population_size,
+            defective_count=int(consumer_risk_point[0] * population_size),
+        )
+        prob_accept_good_lot = hypergeom(
+            sample_size=sample_size,
+            acceptance_number=defects_accepted,
+            population_size=population_size,
+            defective_count=int(producer_risk_point[0] * population_size),
+        )
+
+        if prob_accept_bad_lot > consumer_risk_point[1]:
+            sample_size += 1  # Increase sample size to be more stringent
+        elif prob_accept_good_lot < producer_risk_point[1]:
+            defects_accepted += 1  # Allow one more defect to be more lenient
+        else:
+            # If both checks pass, the plan is good
+            break
+
+    return SingleSamplingPlan(sample_size, defects_accepted)
+
+
+def simulate_single_sampling_plan(
+    population_size: int,
+    actual_defective: int,
+    plan: tuple[int, int],
+    seed: int = 0,
+    rounds: int = 1000,
+):
+    items = list(range(population_size))
+    random.seed(seed)
+
+    accepted_count = 0
+    for i in range(rounds):
+        defective_items = random.sample(items, actual_defective)
+        sample = random.sample(items, plan[0])
+        accepted = len(set(sample) & set(defective_items)) <= plan[1]
+        accepted_count += int(accepted)
+
+    return accepted_count / rounds
+
+
+def plot_plan(
+    population_size: int,
+    plan: SingleSamplingPlan,
+    filename: str,
+):
+    """Plot the Operating Characteristic (OC) probability for a single-stage
+    hypergeometric sampling plan.
+
+    The graph depicts the probability of accepting the lot given the sampling plan.
+    """
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    font = {"size": 14}
+    matplotlib.rc("font", **font)
+
+    defective_count = list(range(500))
+    p_accept = [
+        hypergeom(plan.sample_size, plan.acceptance_number, population_size, defects)
+        for defects in defective_count
+    ]
+    print(
+        f"Sample size: {plan.sample_size}, Acceptance number: {plan.acceptance_number}",
+    )
+
+    plt.plot(
+        defective_count,
+        p_accept,
+        linewidth=4,
+    )
+    plt.xlabel("Actual defect count", fontsize=16)
+    plt.ylabel("Acceptance probability", fontsize=16)
+
+    plt.scatter(
+        [producer_risk_point[0] * population_size],
+        [producer_risk_point[1]],
+        color="red",
+        s=100,
+        zorder=5,
+    )
+    plt.scatter(
+        [consumer_risk_point[0] * population_size],
+        [consumer_risk_point[1]],
+        color="black",
+        s=100,
+        zorder=5,
+    )
+
+    plt.tight_layout()
+    plt.savefig(filename, dpi=300)
+    plt.clf()
+
+
+if __name__ == "__main__":
+
+    population_size = 1000
+
+    # plot two points on the graph
+    producer_risk_point = (0.05, 0.95)  # (defect rate, acceptance probability)
+    consumer_risk_point = (0.15, 0.05)  # (defect rate, acceptance probability)
+    # find the plan that satisfies these points
+    good_plan = find_plan_hypergeom(
+        producer_risk_point,
+        consumer_risk_point,
+        population_size,
+    )
+
+    plot_plan(population_size, good_plan, "good_oc_curve.pdf")
+
+    # This represents a bad plan for the consumer risk point below. The plan
+    # will accept a lot with too many defects.
+    bad_plan = SingleSamplingPlan(
+        sample_size=50,
+        acceptance_number=10,
+    )
+    plot_plan(population_size, bad_plan, "bad_oc_curve.pdf")
---
+++ tips/single_sampling_plan_test.py
@@ -0,0 +1,81 @@
+import hypothesis.strategies as st
+from hypothesis import example, given, settings
+
+from tips.single_sampling_plan import find_plan_hypergeom, simulate_single_sampling_plan
+
+
+def test_simple():
+    producer_risk_point = (0.05, 0.95)
+    consumer_risk_point = (0.15, 0.20)
+    population_size = 500
+
+    plan = find_plan_hypergeom(
+        producer_risk_point,
+        consumer_risk_point,
+        population_size,
+    )
+
+    assert plan.sample_size == 51
+    assert plan.acceptance_number == 5
+
+
+def floats(min_value, max_value):
+    return st.floats(
+        min_value=min_value,
+        max_value=max_value,
+        allow_infinity=False,
+        allow_nan=False,
+        exclude_min=False,
+    )
+
+
+@settings(deadline=None)
+@given(
+    floats(min_value=0.01, max_value=0.10),
+    floats(min_value=0.80, max_value=0.99),
+    floats(min_value=0.01, max_value=0.10),
+    floats(min_value=0.05, max_value=0.20),
+)
+@example(0.05, 0.95, 0.15, 0.20)
+def test_acceptance_threshold(
+    producer_risk_defect_rate,
+    producer_risk_prob,
+    consumer_risk_defect_rate_inc,
+    consumer_risk_prob,
+):
+    # The producer's defect rate is for the prob of rejecting a good lot, while
+    # the consumer's defect rate is for the prob of accepting a bad lot. In
+    # order to make this consistent, the defect rate assumed by the consumer
+    # must be larger than the defect rate assumed by the producer, or else the
+    # plan will trivially require 100% inspection. This materializes as the
+    # hypergeometric function having invalid inputs or else producing a target
+    # sample size that equals the population size.
+    consumer_risk_defect_rate = (
+        producer_risk_defect_rate + consumer_risk_defect_rate_inc
+    )
+
+    producer_risk_point = (producer_risk_defect_rate, producer_risk_prob)
+    consumer_risk_point = (consumer_risk_defect_rate, consumer_risk_prob)
+    population_size = 500
+
+    plan = find_plan_hypergeom(
+        producer_risk_point,
+        consumer_risk_point,
+        population_size,
+    )
+    print(producer_risk_point, consumer_risk_point)
+    print((population_size, plan.sample_size, plan.acceptance_number))
+
+    if plan.sample_size >= population_size:
+        return  # hypothesis picked unreasonable parameters
+
+    proportion_accepted = simulate_single_sampling_plan(
+        population_size=population_size,
+        actual_defective=plan.acceptance_number,
+        plan=(plan.sample_size, plan.acceptance_number),
+        seed=0,
+        rounds=1000,
+    )
+
+    assert proportion_accepted > consumer_risk_point[1]
+    assert (1 - proportion_accepted) < producer_risk_point[1]
