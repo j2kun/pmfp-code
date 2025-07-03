728a33d9e0df1d35e300f2e5b48cf214ccbaa289
Jeremy Kun <j2kun@users.noreply.github.com>
2025-07-03 13:05:13 -0700

    first version of tip with test broken

--- tips/secret_sharing.py
+++ tips/secret_sharing.py
@@ -1,6 +1,58 @@
-def generate_survey_responses():
-    pass
+import sys
+
+import numpy as np
+from mpyc.runtime import mpc
+
+# sample data that would come from parties on different servers
+# columns are: ssn, age, score
+provider0_data = np.array(
+    [
+        [123, 4, 1],
+        [990, 3, 3],
+        [111, 4, 5],
+        [298, 3, 4],
+    ],
+    dtype=np.int16,
+)
+
+provider1_data = np.array(
+    [
+        [394, 2, 2],
+        [983, 4, 2],
+        [452, 2, 3],
+        [399, 3, 5],
+    ],
+    dtype=np.int16,
+)
+
+provider2_data = np.array(
+    [
+        [987, 3, 3],
+        [127, 3, 1],
+        [100, 1, 2],
+        [400, 1, 3],
+    ],
+    dtype=np.int16,
+)
 
 
-def answer_query():
-    pass
+provider_data = [
+    provider0_data,
+    provider1_data,
+    provider2_data,
+]
+
+
+async def main(provider_id: int):
+    secint = mpc.SecInt(16)
+    await mpc.start()
+    provider_shares = mpc.input(secint.array(provider_data[provider_id]))
+    result = mpc.sum(
+        secint(1) for row in provider_shares if row[1] >= 4 and row[2] <= 2
+    )
+    print("Result:", await mpc.output(result))
+    await mpc.shutdown()
+
+
+if __name__ == "__main__":
+    mpc.run(main(int(sys.argv[1])))
--- tips/secret_sharing_test.py
+++ tips/secret_sharing_test.py
@@ -1,2 +1,22 @@
-def test_simple():
-    pass
+import subprocess
+
+
+def test_secret_sharing():
+    processes = [
+        subprocess.Popen(
+            ["python", "tips/secret_sharing.py", f"{i}", "-M3", f"-I{i}"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        for i in range(3)
+    ]
+
+    outputs = [p.communicate() for p in processes]
+    outputs = [
+        [x for x in stdout.split("\n") if "Result:" in x][0]
+        for (stdout, stderr) in outputs
+    ]
+    assert len(outputs) == 3
+    assert outputs[0] == outputs[1] == outputs[2]
+    assert outputs[0] == "Result: 2"
